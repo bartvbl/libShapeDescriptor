924997b9c0322228d776d8c444cb412591829d5c
Bart van Blokland <bartvblokl@gmail.com>
2019-02-09 17:13:42 +0100

    Added missing include

--- src/shapeSearch/gpu/quasiSpinImageGenerator.cuh
+++ src/shapeSearch/gpu/quasiSpinImageGenerator.cuh
@@ -3,4 +3,6 @@
 #include <shapeSearch/common/types/VertexDescriptors.h>
 #include <shapeSearch/gpu/types/DeviceMesh.h>
 
+#include "cuda_runtime.h"
+
 VertexDescriptors createDescriptorsNewstyle(DeviceMesh device_mesh, cudaDeviceProp device_information);

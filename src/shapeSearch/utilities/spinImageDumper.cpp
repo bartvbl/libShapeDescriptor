a31c88ccda87fb8a60f52874505ae89571b292a1
bartvbl <bartvblokl@gmail.com>
2019-03-05 17:08:10 +0100

    Clarified that the spin image dumper takes in descriptors in host memory

--- src/shapeSearch/utilities/spinImageDumper.cpp
+++ src/shapeSearch/utilities/spinImageDumper.cpp
@@ -134,14 +134,14 @@ void performSpinDump(array<spinPixelType> descriptors, std::string imageDestinat
 	}
 }
 
-void dumpImages(array<classicSpinImagePixelType> descriptors, std::string imageDestinationFile, bool logarithmicImage, unsigned int imagesPerRow)
+void dumpImages(array<classicSpinImagePixelType> hostDescriptors, std::string imageDestinationFile, bool logarithmicImage, unsigned int imagesPerRow)
 {
-	performSpinDump<classicSpinImagePixelType>(descriptors, imageDestinationFile, logarithmicImage, imagesPerRow);
+	performSpinDump<classicSpinImagePixelType>(hostDescriptors, imageDestinationFile, logarithmicImage, imagesPerRow);
 }
 
-void dumpImages(array<newSpinImagePixelType> descriptors, std::string imageDestinationFile, bool logarithmicImage, unsigned int imagesPerRow)
+void dumpImages(array<newSpinImagePixelType> hostDescriptors, std::string imageDestinationFile, bool logarithmicImage, unsigned int imagesPerRow)
 {
-	performSpinDump<newSpinImagePixelType> (descriptors, imageDestinationFile, logarithmicImage, imagesPerRow);
+	performSpinDump<newSpinImagePixelType> (hostDescriptors, imageDestinationFile, logarithmicImage, imagesPerRow);
 }
 
 void dumpCompressedImages(array<unsigned int> compressedDescriptors, std::string imageDestinationFile, bool logarithmicImage, unsigned int imagesPerRow) {

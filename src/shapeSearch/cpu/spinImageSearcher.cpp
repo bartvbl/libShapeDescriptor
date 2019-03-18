a6640f3b81de643f5998584fcd9165e8a7c217e5
bartvbl <bartvblokl@gmail.com>
2019-03-18 15:49:57 +0100

    Completed implementation of CPU-based image comparison algorithm

--- src/shapeSearch/cpu/spinImageSearcher.cpp
+++ src/shapeSearch/cpu/spinImageSearcher.cpp
@@ -1,9 +1,90 @@
+#include <cmath>
+#include <algorithm>
+#include <iostream>
 #include "spinImageSearcher.h"
 
-template<typename pixelType>
-float computeCorrelations(const array<pixelType> &needleDescriptors, const array<pixelType> &haystackDescriptors,
-                          size_t image, size_t haystackImage) {
+bool compareSearchResults(const DescriptorSearchResult &a, const DescriptorSearchResult &b)
+{
+    return a.correlation > b.correlation;
+}
 
+template<typename pixelType>
+float computeImagePairCorrelation(pixelType* descriptors,
+                                  pixelType* otherDescriptors,
+                                  size_t spinImageIndex,
+                                  size_t otherImageIndex,
+                                  float averageX, float averageY) {
+    float squaredSumX = 0;
+    float squaredSumY = 0;
+    float multiplicativeSum = 0;
+
+    for (int y = 0; y < spinImageWidthPixels; y++)
+    {
+        for (int x = 0; x < spinImageWidthPixels; x++)
+        {
+            const size_t spinImageElementCount = spinImageWidthPixels * spinImageWidthPixels;
+
+            pixelType pixelValueX = descriptors[spinImageIndex * spinImageElementCount + (y * spinImageWidthPixels + x)];
+            pixelType pixelValueY = otherDescriptors[otherImageIndex * spinImageElementCount + (y * spinImageWidthPixels + x)];
+
+            float deltaX = float(pixelValueX) - averageX;
+            float deltaY = float(pixelValueY) - averageY;
+
+            squaredSumX += deltaX * deltaX;
+            squaredSumY += deltaY * deltaY;
+            multiplicativeSum += deltaX * deltaY;
+        }
+    }
+
+    squaredSumX = std::sqrt(squaredSumX);
+    squaredSumY = std::sqrt(squaredSumY);
+
+    float correlation = -1;
+
+    if(squaredSumX != 0 || squaredSumY != 0)
+    {
+        // Avoiding zero divisions
+        const float smallestNonZeroFactor = 0.0001;
+        squaredSumX = std::max(squaredSumX, smallestNonZeroFactor);
+        squaredSumY = std::max(squaredSumY, smallestNonZeroFactor);
+        multiplicativeSum = std::max(multiplicativeSum, smallestNonZeroFactor * smallestNonZeroFactor);
+
+        correlation = multiplicativeSum / (squaredSumX * squaredSumY);
+    } else if(squaredSumX == 0 && squaredSumY == 0) {
+        // If both sums are 0, both sequences must be identical
+        correlation = 1;
+    }
+
+    return correlation;
+}
+
+template<typename pixelType>
+float computeImageAverage(pixelType* descriptors, size_t spinImageIndex)
+{
+    const unsigned int spinImageElementCount = spinImageWidthPixels * spinImageWidthPixels;
+
+    float sum = 0;
+
+    for (int y = 0; y < spinImageWidthPixels; y++)
+    {
+        for (int x = 0; x < spinImageWidthPixels; x ++)
+        {
+            float pixelValue = float(descriptors[spinImageIndex * spinImageElementCount + (y * spinImageWidthPixels + x)]);
+            sum += pixelValue;
+        }
+    }
+
+    return sum / float(spinImageElementCount);
+}
+
+template<typename pixelType>
+float computeCorrelation(const array<pixelType> &needleDescriptors, const array<pixelType> &haystackDescriptors,
+                          size_t needleImageIndex, size_t haystackImageIndex) {
+    float needleAverage = computeImageAverage<pixelType>(needleDescriptors.content, needleImageIndex);
+    float haystackAverage = computeImageAverage<pixelType>(haystackDescriptors.content, haystackImageIndex);
+
+    return computeImagePairCorrelation<pixelType>(needleDescriptors.content,
+            haystackDescriptors.content, needleImageIndex, haystackImageIndex, needleAverage, haystackAverage);
 }
 
 template<typename pixelType>
@@ -15,13 +96,12 @@ std::vector<std::vector<DescriptorSearchResult>> computeCorrelations(
 
     std::vector<std::vector<DescriptorSearchResult>> searchResults;
     searchResults.resize(needleImageCount);
-
 #pragma omp parallel for
     for(size_t image = 0; image < needleImageCount; image++) {
         std::vector<DescriptorSearchResult> imageResults;
 
         for(size_t haystackImage = 0; haystackImage < haystackImageCount; haystackImage++) {
-            float correlation = computeCorrelations<pixelType>(needleDescriptors, haystackDescriptors, image, haystackImage);
+            float correlation = computeCorrelation<pixelType>(needleDescriptors, haystackDescriptors, image, haystackImage);
 
             DescriptorSearchResult entry;
             entry.correlation = correlation;
@@ -29,8 +109,11 @@ std::vector<std::vector<DescriptorSearchResult>> computeCorrelations(
             imageResults.push_back(entry);
         }
 
-        searchResults.push_back(imageResults);
+        std::sort(imageResults.begin(), imageResults.end(), compareSearchResults);
+        searchResults.at(image) = imageResults;
     }
+
+    std::cout << "Analysed " << searchResults.size() << " images on the CPU." << std::endl;
 
     return searchResults;
 }

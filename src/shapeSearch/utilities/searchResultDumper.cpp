6bfa89a5577ea22fcd7dd9df42b0f7b4f12f61e9
bartvbl <bartvblokl@gmail.com>
2019-03-18 15:49:30 +0100

    Added dump function for CPU-generated search results

--- src/shapeSearch/utilities/searchResultDumper.cpp
+++ src/shapeSearch/utilities/searchResultDumper.cpp
@@ -20,3 +20,25 @@ void dumpSearchResults(array<ImageSearchResults> searchResults, size_t imageCoun
 
     outputFile.close();
 }
+
+void dumpSearchResults(std::vector<std::vector<DescriptorSearchResult>> searchResults, std::string outputFilePath) {
+    std::ofstream outputFile;
+    outputFile.open(outputFilePath);
+
+    for(size_t image = 0; image < searchResults.size(); image++) {
+        size_t resultCount = searchResults.at(image).size();
+
+        outputFile << "----- Image " << image << " -----" << std::endl;
+        outputFile << "Scores: ";
+        for (unsigned int i = 0; i < resultCount; i++) {
+            outputFile << searchResults.at(image).at(i).correlation << (i == resultCount-1 ? "\r\n" : ", ");
+        }
+        outputFile << "Indices: ";
+        for (unsigned int i = 0; i < resultCount; i++) {
+            outputFile << searchResults.at(image).at(i).imageIndex << (i == resultCount-1 ? "\r\n" : ", ");
+        }
+        outputFile << "\r\n";
+    }
+
+    outputFile.close();
+}

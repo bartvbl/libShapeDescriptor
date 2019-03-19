53c015bf9680ef89f1109156ef63a355fc17efba
bartvbl <bartvblokl@gmail.com>
2019-03-19 18:12:41 +0100

    Getting ready to implement correlation test cases

--- tests/correlationComputations.cpp
+++ tests/correlationComputations.cpp
@@ -1,11 +1,7 @@
 #include "correlationComputations.h"
 #include <catch2/catch.hpp>
-#include <shapeSearch/cpu/types/float3_cpu.h>
 
-TEST_CASE("float3 length", "Description?" ) {
-    float3_cpu vertex;
-    vertex.x = 1;
-    vertex.y = 0;
-    vertex.z = 0;
-    REQUIRE(length(vertex) == 1);
+
+TEST_CASE("Correlation computation", "[correlation]") {
+
 }
--- tests/testMain.cpp
+++ tests/testMain.cpp
@@ -1,5 +1,6 @@
 #include "testMain.h"
 #include <shapeSearch/cpu/types/float2_cpu.h>
+#include <shapeSearch/cpu/types/float3_cpu.h>
 
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
@@ -7,9 +8,20 @@
 
 
 
-TEST_CASE("float2 length", "Description?" ) {
-    float2_cpu vertex;
-    vertex.x = 1;
-    vertex.y = 0;
-    REQUIRE(length(vertex) == 1);
+TEST_CASE("float vector structs", "[vectors]" ) {
+
+    SECTION("float2 length") {
+        float2_cpu vertex;
+        vertex.x = 1;
+        vertex.y = 0;
+        REQUIRE(length(vertex) == 1);
+    }
+
+    SECTION("float3 length") {
+        float3_cpu vertex;
+        vertex.x = 1;
+        vertex.y = 0;
+        vertex.z = 0;
+        REQUIRE(length(vertex) == 1);
+    }
 }

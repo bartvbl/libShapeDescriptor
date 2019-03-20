7fa6c1a6bcc2ae3e61ff8eb70b6732bb6c6b860f
bartvbl <bartvblokl@gmail.com>
2019-03-20 15:29:50 +0100

    Put all host/device transfer functions in the same place

--- src/shapeSearch/gpu/copy/copyDescriptorsToHost.h
+++ src/shapeSearch/gpu/copy/copyDescriptorsToHost.h
@@ -3,5 +3,6 @@
 #include <shapeSearch/common/types/array.h>
 #include <shapeSearch/libraryBuildSettings.h>
 
+
 array<newSpinImagePixelType> copyQSIDescriptorsToHost(array<newSpinImagePixelType> device_descriptors, size_t imageCount);
 array<classicSpinImagePixelType> copySpinImageDescriptorsToHost(array<classicSpinImagePixelType> device_descriptors, size_t imageCount);

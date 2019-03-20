deb4ed82727842e409f0b290d3a99820ae8bb95b
bartvbl <bartvblokl@gmail.com>
2019-03-20 15:44:48 +0100

    Renamed copy files

--- src/shapeSearch/gpu/copy/CopyMeshHostToDevice.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <shapeSearch/cpu/types/HostMesh.h>
-#include <shapeSearch/gpu/types/DeviceMesh.h>
-
-namespace SpinImage::copy {
-    DeviceMesh hostMeshToDevice(HostMesh hostMesh);
-}

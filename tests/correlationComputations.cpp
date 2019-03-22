d6f0b297ec473d11f2ce213d0a75d2bda9f1cdce
bartvbl <bartvblokl@gmail.com>
2019-03-22 14:04:20 +0100

    Added test for correlation where two sequences are perfectly negatively correlated

--- tests/correlationComputations.cpp
+++ tests/correlationComputations.cpp
@@ -9,6 +9,16 @@
 #include <nvidia/helper_cuda.h>
 
 template<typename pixelType>
+array<pixelType> generateEmptyImages(size_t imageCount) {
+    pixelType* image = new pixelType[imageCount * spinImageWidthPixels * spinImageWidthPixels];
+    array<pixelType> images;
+    images.content = image;
+    images.length = imageCount * spinImageWidthPixels * spinImageWidthPixels;
+
+    return images;
+}
+
+template<typename pixelType>
 array<pixelType> generateRepeatingTemplateImage(
         pixelType patternPart0,
         pixelType patternPart1,
@@ -19,21 +29,19 @@ array<pixelType> generateRepeatingTemplateImage(
         pixelType patternPart6,
         pixelType patternPart7) {
 
-    pixelType* image = new pixelType[spinImageWidthPixels * spinImageWidthPixels];
+    array<pixelType> images = generateEmptyImages<pixelType>(1);
 
     for(size_t index = 0; index < spinImageWidthPixels * spinImageWidthPixels; index += 8) {
-        image[index + 0] = patternPart0;
-        image[index + 1] = patternPart1;
-        image[index + 2] = patternPart2;
-        image[index + 3] = patternPart3;
-        image[index + 4] = patternPart4;
-        image[index + 5] = patternPart5;
-        image[index + 6] = patternPart6;
-        image[index + 7] = patternPart7;
+        images.content[index + 0] = patternPart0;
+        images.content[index + 1] = patternPart1;
+        images.content[index + 2] = patternPart2;
+        images.content[index + 3] = patternPart3;
+        images.content[index + 4] = patternPart4;
+        images.content[index + 5] = patternPart5;
+        images.content[index + 6] = patternPart6;
+        images.content[index + 7] = patternPart7;
     }
-    array<pixelType> images;
-    images.content = image;
-    images.length = spinImageWidthPixels * spinImageWidthPixels;
+
     return images;
 }
 
@@ -46,7 +54,27 @@ TEST_CASE("Correlation computation", "[correlation]") {
 
         float correlation = SpinImage::cpu::computeImagePairCorrelation(constantImage.content, constantImage.content, 0, 0);
 
+        delete[] constantImage.content;
         REQUIRE(correlation == 1);
     }
+
+    SECTION("Opposite images") {
+        array<spinImagePixelType> positiveImage = generateEmptyImages<spinImagePixelType>(1);
+        array<spinImagePixelType> negativeImage = generateEmptyImages<spinImagePixelType>(1);
+
+        for(int i = 0; i < spinImageWidthPixels * spinImageWidthPixels; i++) {
+            positiveImage.content[i] = float(i);
+            negativeImage.content[i] = float(i) * -1;
+        }
+
+        float correlation = SpinImage::cpu::computeImagePairCorrelation(positiveImage.content, negativeImage.content, 0,
+                                                                        0);
+
+        delete[] positiveImage.content;
+        delete[] negativeImage.content;
+        REQUIRE(correlation == -1);
+    }
+
+
 
 }

484b2990ed63466bd674d65d0b040d23a0149ecc
bartvbl <bartvblokl@gmail.com>
2019-04-02 11:53:55 +0200

    Removed unimplemented CPU QSI distance function

--- src/spinImage/cpu/QSIDistanceFunction.cpp
+++
@@ -1,6 +0,0 @@
-#include <spinImage/common/types/array.h>
-#include "QSIDistanceFunction.h"
-
-float SpinImage::cpu::computeQuasiSpinImageDistance(unsigned int *referenceImage, unsigned int *sampleImage) {
-
-}
--- src/spinImage/cpu/QSIDistanceFunction.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace SpinImage {
-    namespace cpu {
-        float computeQuasiSpinImageDistance(unsigned int *referenceImage, unsigned int *sampleImage);
-    }
-}

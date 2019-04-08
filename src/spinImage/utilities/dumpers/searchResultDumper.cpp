f9f508d95e258e38ec8d21007aef9b2bcfc171d5
bartvbl <bartvblokl@gmail.com>
2019-04-08 16:12:57 +0200

    Added dumper function for QSI search results

--- src/spinImage/utilities/dumpers/searchResultDumper.cpp
+++ src/spinImage/utilities/dumpers/searchResultDumper.cpp
@@ -1,7 +1,27 @@
 #include <fstream>
 #include "searchResultDumper.h"
 
-void SpinImage::dump::searchResults(array<ImageSearchResults> searchResults, size_t imageCount, std::string outputFilePath) {
+void SpinImage::dump::searchResults(array<SpinImageSearchResults> searchResults, size_t imageCount, std::string outputFilePath) {
+    std::ofstream outputFile;
+    outputFile.open(outputFilePath);
+
+    for(size_t image = 0; image < imageCount; image++) {
+        outputFile << "----- Image " << image << " -----" << std::endl;
+        outputFile << "Scores: ";
+        for (unsigned int i = 0; i < SEARCH_RESULT_COUNT; i++) {
+            outputFile << searchResults.content[image].resultScores[i] << (i == SEARCH_RESULT_COUNT-1 ? "\r\n" : ", ");
+        }
+        outputFile << "Indices: ";
+        for (unsigned int i = 0; i < SEARCH_RESULT_COUNT; i++) {
+            outputFile << searchResults.content[image].resultIndices[i] << (i == SEARCH_RESULT_COUNT-1 ? "\r\n" : ", ");
+        }
+        outputFile << "\r\n";
+    }
+
+    outputFile.close();
+}
+
+void SpinImage::dump::searchResults(array<QuasiSpinImageSearchResults> searchResults, size_t imageCount, std::string outputFilePath) {
     std::ofstream outputFile;
     outputFile.open(outputFilePath);
 

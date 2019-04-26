3c6bae7c235b3aca11b178ddc75f8a8d2168f448
bartvbl <bartvblokl@gmail.com>
2019-04-26 14:49:07 +0200

    Added a function for dumping meshes to OBJ files

---
+++ src/spinImage/utilities/dumpers/meshDumper.cpp
@@ -0,0 +1,29 @@
+#include "meshDumper.h"
+#include <fstream>
+#include <iostream>
+
+void SpinImage::dump::mesh(HostMesh mesh, std::string outputFilePath) {
+    std::ofstream outputFile;
+    outputFile.open(outputFilePath);
+
+    for(unsigned int i = 0; i < mesh.vertexCount; i++) {
+        outputFile << "v " << mesh.vertices[i].x << " " << mesh.vertices[i].y << " " <<mesh.vertices[i].z << std::endl;
+    }
+
+    outputFile << std::endl;
+
+    for(unsigned int i = 0; i < mesh.vertexCount; i++) {
+        outputFile << "vn " << mesh.normals[i].x << " " << mesh.normals[i].y << " " <<mesh.normals[i].z << std::endl;
+    }
+
+    outputFile << std::endl;
+    outputFile << "o object" << std::endl;
+    outputFile << "g object" << std::endl;
+    outputFile << std::endl;
+
+    for(unsigned int i = 0; i < mesh.vertexCount; i += 3) {
+        outputFile << "f " << (i+1) << "//" << (i+1) << " " << (i+2) << "//" << (i+2) << " " << (i+3) << "//" << (i+3) << std::endl;
+    }
+
+    outputFile.close();
+}
---
+++ src/spinImage/utilities/dumpers/meshDumper.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <spinImage/cpu/types/HostMesh.h>
+
+namespace SpinImage {
+    namespace dump {
+        void mesh(HostMesh mesh, std::string outputFile);
+    }
+}

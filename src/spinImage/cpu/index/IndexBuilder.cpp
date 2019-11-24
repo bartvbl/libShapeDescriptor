7af436461bd45e44e9265a8f779c87f93bd266dd
Bart van Blokland <bartvblokl@gmail.com>
2019-11-24 21:32:13 +0100

    Fixed linker errors related to use of std::filesystem

--- src/spinImage/cpu/index/IndexBuilder.cpp
+++ src/spinImage/cpu/index/IndexBuilder.cpp
@@ -1,5 +1,11 @@
+#include <spinImage/utilities/fileutils.h>
+#include <iostream>
 #include "IndexBuilder.h"
 
 Index SpinImage::index::build(std::string quicciImageDumpDirectory, std::string indexDumpDirectory) {
-    //std::vector<std::string> filesInDirectory = listdir(quicciImageDumpDirectory);
+    std::vector<std::experimental::filesystem::path> filesInDirectory = SpinImage::utilities::listDirectory(quicciImageDumpDirectory);
+
+    for(auto &path : filesInDirectory) {
+        std::cout << path.filename() << std::endl;
+    }
 }

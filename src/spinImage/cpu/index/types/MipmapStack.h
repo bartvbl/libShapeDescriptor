5773b3f7f30a1f9cb8fe09c7200b00090eb99dae
Bart van Blokland <bartvblokl@gmail.com>
2019-12-05 01:51:09 +0100

    Reworked the mipmap stack. Each level is now a separate struct, and computed more efficiently

--- src/spinImage/cpu/index/types/MipmapStack.h
+++ src/spinImage/cpu/index/types/MipmapStack.h
@@ -5,123 +5,169 @@
 
 constexpr int uintsPerRow = spinImageWidthPixels / 32;
 
-// bits per pixel:                     2 bits      4 bits      8 bits      16 bits     32 bits
-const unsigned int collatingShiftBitMasks[5] = {0x55555555, 0x11111111, 0x01010101, 0x00010001, 0x00000001};
-
 // bits per pixel:                                   2 -> 4      4 -> 8      8 -> 16     16 -> 32
 const unsigned int groupingShiftBitMasks_2bits[4] = {0x33333333, 0x0F0F0F0F, 0x00FF00FF, 0x0000FFFF};
-const unsigned int groupingShiftBitMasks_4bits[3] = {            0x03030303, 0x000F000F, 0x000000FF};
-const unsigned int groupingShiftBitMasks_8bits[2] = {                        0x00030003, 0x0000000F};
-const unsigned int groupingShiftBitMasks_16bits[1]= {                                    0x00000003};
+
+// Shift all bits to the end, so they are all collected together.
+inline unsigned int compressChunk_2bits(unsigned int chunk) {
+    unsigned int shiftIndex = 0;
+    for(unsigned int shiftDistance = 1; shiftDistance < 16; shiftDistance *= 2) {
+        chunk = (chunk | (chunk >> shiftDistance)) & groupingShiftBitMasks_2bits[shiftIndex];
+        shiftIndex++;
+    }
+    return chunk;
+}
+
+struct MipMapImage {
+    unsigned int compare(const MipMapImage other, const unsigned int) {
+
+    }
+};
+
+struct MipMapLevel3 {
+    unsigned int image[32];
+
+    // 64x64 -> 32x32 image
+    static void computeMipmapLevel3(unsigned int* level3, const unsigned int* quiccImage) {
+        for(int row = 0; row < spinImageWidthPixels; row += 2) {
+            unsigned int topLeftChunk = quiccImage[row * uintsPerRow + 0];
+            unsigned int topRightChunk = quiccImage[row * uintsPerRow + 1];
+            unsigned int bottomLeftChunk = quiccImage[(row + 1) * uintsPerRow + 0];
+            unsigned int bottomRightChunk = quiccImage[(row + 1) * uintsPerRow + 1];
+
+            unsigned int topLeftCombined = (topLeftChunk | (topLeftChunk >> 1U)) & 0x55555555U;
+            unsigned int bottomLeftCombined = (bottomLeftChunk | (bottomLeftChunk >> 1U)) & 0x55555555U;
+            unsigned int topRightCombined = (topRightChunk | (topRightChunk >> 1U)) & 0x55555555U;
+            unsigned int bottomRightCombined = (bottomRightChunk | (bottomRightChunk >> 1U)) & 0x55555555U;
+
+            unsigned int compressedLeftChunk = compressChunk_2bits(topLeftCombined | bottomLeftCombined);
+            unsigned int compressedRightChunk = compressChunk_2bits(topRightCombined | bottomRightCombined);
+
+            level3[row / 2] = (compressedLeftChunk << 16U) | compressedRightChunk;
+        }
+    }
+
+    MipMapLevel3(unsigned int* quicciImage) {
+        computeMipmapLevel3(image, quicciImage);
+    }
+};
+
+struct MipMapLevel2 {
+    unsigned int image[8];
+
+    // 32x32 -> 16x16 image
+    static void computeMipmapLevel2(unsigned int* level2, const unsigned int* level3) {
+        unsigned int combinedCompressedChunk = 0;
+
+        for(unsigned int row = 0; row < 32; row += 2) {
+            unsigned int topChunk = level3[row];
+            unsigned int bottomChunk = level3[row + 1];
+
+            unsigned int topCombined = (topChunk | (topChunk >> 1U)) & 0x55555555U;
+            unsigned int bottomCombined = (bottomChunk | (bottomChunk >> 1U)) & 0x55555555U;
+
+            unsigned int compressedChunk = compressChunk_2bits(topCombined | bottomCombined);
+
+            // We need 2 16 bit compressed chunks to make 1 32-bit output chunk.
+            // So we save every other of these chunks, and only write when we need to.
+            if((row & 0x2U) == 0) {
+                combinedCompressedChunk = (compressedChunk << 16U);
+            } else {
+                level2[row / 4] = combinedCompressedChunk | compressedChunk;
+            }
+        }
+    }
+
+    MipMapLevel2(MipMapLevel3 level3Image) {
+        computeMipmapLevel2(image, level3Image.image);
+    }
+};
+
+struct MipMapLevel1 {
+    const std::array<unsigned int, 2> image;
+
+    // 16x16 -> 8x8 image
+    static std::array<unsigned int, 2> computeMipmapLevel1(const unsigned int* level2) {
+        unsigned int level1[2];
+
+        unsigned int combinedCompressedChunk = 0;
+        unsigned char byteIndex = 0;
+
+        for(unsigned int chunk = 0; chunk < 8; chunk++) {
+            unsigned int doubleRowChunk = level2[chunk];
+
+            unsigned int combined = (doubleRowChunk | (doubleRowChunk >> 1U)) & 0x55555555U;
+            combined = (combined | (combined >> 16U)) & 0x00005555U;
+
+            unsigned int compressedChunk = compressChunk_2bits(combined);
+
+            // Every 4 chunks produces one output chunk
+            // (8 bits per processed chunk)
+            combinedCompressedChunk = combinedCompressedChunk | (compressedChunk << (32U - 8U - 8U*byteIndex));
+            if(byteIndex == 3) {
+                level1[chunk / 4] = combinedCompressedChunk;
+                combinedCompressedChunk = 0;
+                byteIndex = 0;
+            } else {
+                byteIndex++;
+            }
+        }
+        return level1;
+    }
+
+    MipMapLevel1(MipMapLevel2 higherLevelImage) {
+        computeMipmapLevel1(image, higherLevelImage.image);
+    }
+};
+
+struct MipMapLevel0 {
+    const unsigned short image;
+
+    static const unsigned short computeMipmapLevel0(MipMapLevel1 level1) {
+        unsigned int partialChunks[2] = {0, 0};
+
+        for(int i = 0; i < 2; i++) {
+            unsigned int chunk = level1.image[i];
+            // Combine pixels horizontally
+            chunk = (chunk | (chunk >> 1U)) & 0x55555555U;
+            // Combine pixels vertically
+            chunk = (chunk | (chunk >> 8U)) & 0x00550055U;
+            // Shift pixel bits together
+            chunk = (chunk | (chunk >> 8U)) & 0x00005555U;
+            // Compress like a regular 2-bit distanced chunk
+            // Leaves the final 8 bits populated.
+            partialChunks[i] = compressChunk_2bits(chunk);
+        }
+
+        // Both chunks produce 8 bits each, so we combine them into an unsigned short here.
+        return (unsigned short) ((partialChunks[0] << 8U) | partialChunks[1]);
+    }
+
+    MipMapLevel0(MipMapLevel1 level1) : image(computeMipmapLevel0(level1)) {
+
+    }
+};
 
 struct MipmapStack {
-    //   level   mipmap size    pixel count   area per pixel   value range   space needed
-    //   0       4x4 images     16            16x16 pixels     0-256         16 bytes, 8 bits/pixel
-    //   1       8x8 images     64            8x8 pixels       0-64          64 bytes, 8 (6) bits/pixel
-    //   2       16x16 images   256           4x4 pixels       0-16          128 bytes, 4 bits/pixel
-    //   3       32x32 images   1024          2x2 pixels       0-4           256 bytes, 2 bits/pixel
+    //   level   mipmap size    pixel count   area per pixel   space needed
+    //   0       4x4 images     16            16x16 pixels     1 unsigned short
+    //   1       8x8 images     64            8x8 pixels       2 unsigned ints
+    //   2       16x16 images   256           4x4 pixels       8 unsigned ints
+    //   3       32x32 images   1024          2x2 pixels       32 unsigned ints
     //   -- 64x64: source --
 
-    //   level   mipmap size    pixel count   space needed
-    //   0       4x4 images     16            1 unsigned short
-    //   1       8x8 images     64            2 unsigned ints
-    //   2       16x16 images   256           8 unsigned ints
-    //   3       32x32 images   1024          32 unsigned ints
-    //   -- 64x64: source --
+    // The order in which these are defined matters due to the initialiser list of the constructor
+    MipMapLevel3 level3;
+    MipMapLevel2 level2;
+    MipMapLevel1 level1;
+    MipMapLevel0 level0;
 
-    unsigned short level0;
-    unsigned int level1[2];
-    unsigned int level2[8];
-    unsigned int level3[32];
-
-    inline unsigned int compressChunk(
-            unsigned int chunk,
-            const unsigned int bitsPerPixel,
-            const unsigned int* groupingBitShiftMasks,
-            const unsigned int initialBitGroupingShiftDistance) {
-        // First we shift and OR bits together to collect bits together
-        int shiftIndex = 0;
-        for(unsigned int shiftDistance = 1; shiftDistance < bitsPerPixel; shiftDistance *= 2) {
-            chunk = (chunk | (chunk >> shiftDistance)) & collatingShiftBitMasks[shiftIndex];
-            //             ^ The operation applied for collecting the bits
-            shiftIndex++;
-        }
-        // Next we shift all bits to the end, so they are all collected together.
-        // Note that the final bit is always already in position, hence we start with the second-to-last
-        shiftIndex = 0;
-        unsigned int bitGroupingShiftDistance = initialBitGroupingShiftDistance;
-        for(unsigned int shiftDistance = bitsPerPixel; shiftDistance < 32; shiftDistance *= 2) {
-            chunk = (chunk | (chunk >> bitGroupingShiftDistance)) & groupingBitShiftMasks[shiftIndex];
-            shiftIndex++;
-            bitGroupingShiftDistance *= 2;
-        }
-        return chunk;
-    }
-
-    void computeMipmapLevel(
-            unsigned int* mipmapLevelImage,
-            unsigned int *quiccImage,
-            const unsigned int mipmapBlockSizeBits,
-            const unsigned int* groupingBitShiftMasks,
-            const unsigned int initialBitGroupingShiftDistance) {
-        unsigned int mipmapChunkIndex = 0;
-
-        const unsigned int bitsPerMipmapRow = spinImageWidthPixels / mipmapBlockSizeBits;
-        const unsigned int totalBitCount = bitsPerMipmapRow * bitsPerMipmapRow;
-        const unsigned int bitsPerUint = 32 / mipmapBlockSizeBits;
-
-        unsigned int currentChunk = 0;
-        unsigned int accumulatedBitCount = 0;
-        for(unsigned int rowChunk = 0; rowChunk < spinImageWidthPixels / mipmapBlockSizeBits; rowChunk++) {
-            for(unsigned int col = 0; col < uintsPerRow; col++) {
-                unsigned int collatedRows = 0;
-
-                for(unsigned int row = 0; row < mipmapBlockSizeBits; row++) {
-                    unsigned int chunk = quiccImage[(rowChunk * mipmapBlockSizeBits + row) * uintsPerRow + col];
-                    unsigned int shiftIndex = 0;
-                    for(unsigned int shiftDistance = 1; shiftDistance < mipmapBlockSizeBits; shiftDistance *= 2) {
-                        // Horizontally merge bits together that belong to the same mipmap pixel/block
-                        chunk = (chunk | (chunk >> shiftDistance)) & collatingShiftBitMasks[shiftIndex];
-                        shiftIndex++;
-                    }
-                    // Vertically merge bits together
-                    collatedRows = collatedRows | chunk;
-                }
-
-                // Bits are still distanced apart.
-                // We first need to bring them together before we can shift them in place
-                unsigned int compressedChunk = compressChunk(collatedRows, mipmapBlockSizeBits, groupingBitShiftMasks, initialBitGroupingShiftDistance);
-
-                currentChunk = currentChunk | (compressedChunk << (32 - bitsPerUint - accumulatedBitCount));
-                accumulatedBitCount += bitsPerUint;
-
-                if(accumulatedBitCount == 32) {
-                    mipmapLevelImage[mipmapChunkIndex] = currentChunk;
-                    mipmapChunkIndex++;
-
-                    // Reset the chunk accumulator
-                    accumulatedBitCount = 0;
-                    currentChunk = 0;
-                }
-            }
-            // HACK: for computing level 0, we're dealing with a single unsigned short.
-            if(totalBitCount == 16) {
-                ((unsigned short*) mipmapLevelImage)[mipmapChunkIndex] = (unsigned short) (currentChunk >> 16);
-            }
-        }
-    }
-
-    MipmapStack(unsigned int* quiccImage) {
+    MipmapStack(unsigned int* quiccImage) :
+            level3(quiccImage),
+            level2(level3),
+            level1(level2),
+            level0(level1) {
         static_assert(spinImageWidthPixels == 64);
-
-        // While a giant hack, the computeMipmapLevel() function will detect the case where it deals with
-        // an unsigned short as input, and will account for it. In the interest of reusing code this seemed
-        // the best solution.
-
-        computeMipmapLevel((unsigned int*) &level0, quiccImage, 16, groupingShiftBitMasks_16bits, 15);
-        computeMipmapLevel(level1, quiccImage, 8, groupingShiftBitMasks_8bits, 7);
-        computeMipmapLevel(level2, quiccImage, 4, groupingShiftBitMasks_4bits, 3);
-        computeMipmapLevel(level3, quiccImage, 2, groupingShiftBitMasks_2bits, 1);
     }
 
     template<typename printedType> void printBitwiseImage(printedType* image, int size) {
@@ -144,15 +190,15 @@ struct MipmapStack {
 
     void print() {
         std::cout << "Level 0" << std::endl;
-        printBitwiseImage<unsigned short>(&level0, 4);
+        printBitwiseImage<unsigned short>(&level0.image, 4);
 
         std::cout << std::endl << "Level 1" << std::endl;
-        printBitwiseImage<unsigned int>(level1, 8);
+        printBitwiseImage<unsigned int>(level1.image, 8);
 
         std::cout << std::endl << "Level 2" << std::endl;
-        printBitwiseImage<unsigned int>(level2, 16);
+        printBitwiseImage<unsigned int>(level2.image, 16);
 
         std::cout << std::endl << "Level 3" << std::endl;
-        printBitwiseImage<unsigned int>(level3, 32);
+        printBitwiseImage<unsigned int>(level3.image, 32);
     }
 };

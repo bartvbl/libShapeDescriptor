daeeed7facfd2355d3c0d27c9b0e5d6d8cc95607
Bart van Blokland <bartvblokl@gmail.com>
2019-12-16 01:44:18 +0100

    Computing a pixel count for each image type

--- src/spinImage/cpu/index/IndexBuilder.cpp
+++ src/spinImage/cpu/index/IndexBuilder.cpp
@@ -47,7 +47,7 @@ Index SpinImage::index::build(std::string quicciImageDumpDirectory, std::string 
     std::experimental::filesystem::path indexDirectory(indexDumpDirectory);
 
     // The index node capacity is set quite high to allow most of the index to be in memory during construction
-    IndexFileCache cache(indexDirectory, 65536 * 64, 65536 * 32, 50000);
+    IndexFileCache cache(indexDirectory, 65536 * 32, 65536 * 24, 50000);
 
     std::vector<size_t> indexNodesPerRootNode;
     std::vector<size_t> bucketNodesPerRootNode;
@@ -60,6 +60,14 @@ Index SpinImage::index::build(std::string quicciImageDumpDirectory, std::string 
 
     std::vector<std::experimental::filesystem::path>* indexedFiles = new std::vector<std::experimental::filesystem::path>();
     indexedFiles->reserve(5000);
+
+    std::array<unsigned long long, spinImageWidthPixels * spinImageWidthPixels> increasingCounts;
+    std::array<unsigned long long, spinImageWidthPixels * spinImageWidthPixels> decreasingCounts;
+
+    for(int i = 0; i < spinImageWidthPixels * spinImageWidthPixels; i++) {
+        increasingCounts[i] = 0;
+        decreasingCounts[i] = 0;
+    }
 
     const unsigned int uintsPerQUICCImage = (spinImageWidthPixels * spinImageWidthPixels) / 32;
     IndexFileID fileIndex = 0;
@@ -74,7 +82,7 @@ Index SpinImage::index::build(std::string quicciImageDumpDirectory, std::string 
 
 
         std::chrono::steady_clock::time_point startTime = std::chrono::steady_clock::now();
-//#pragma omp parallel for
+#pragma omp parallel for
         for(IndexImageID image = 0; image < images.imageCount; image++) {
             //MipmapStack mipmapsIncreasing(images.horizontallyIncreasingImages + image * uintsPerQUICCImage);
             //MipmapStack mipmapsDecreasing(images.horizontallyDecreasingImages + image * uintsPerQUICCImage);
@@ -83,7 +91,26 @@ Index SpinImage::index::build(std::string quicciImageDumpDirectory, std::string 
                     images.horizontallyIncreasingImages + image * uintsPerQUICCImage,
                     images.horizontallyDecreasingImages + image * uintsPerQUICCImage);
 
-            IndexNodeID nextLink = cache.rootNode.links.at(combined.level0.image);
+            unsigned int bitIndex = 0;
+            unsigned int byteIndex = 0;
+            const unsigned int bitsPerType = sizeof(unsigned int) * 8;
+            for(int row = 0; row < 64; row++) {
+                for(int col = 0; col < 64; col++) {
+                    unsigned int increasingBits = images.horizontallyIncreasingImages[byteIndex];
+                    unsigned int decreasingBits = images.horizontallyDecreasingImages[byteIndex];
+#pragma omp atomic
+                    increasingCounts[row * spinImageWidthPixels + col] += ((increasingBits >> (bitsPerType - 1U - bitIndex)) & 0x1U);
+#pragma omp atomic
+                    decreasingCounts[row * spinImageWidthPixels + col] += ((decreasingBits >> (bitsPerType - 1U - bitIndex)) & 0x1U);
+                    bitIndex++;
+                    if(bitIndex == bitsPerType) {
+                        byteIndex++;
+                        bitIndex = 0;
+                    }
+                }
+            }
+
+            /*IndexNodeID nextLink = cache.rootNode.links.at(combined.level0.image);
             if(nextLink == ROOT_NODE_LINK_DISABLED) {
                 // Temporarily expand the 16-bit root image to avoid pointer cast shenanigans
                 unsigned int rootMipmapImage = combined.level0.image;
@@ -97,23 +124,23 @@ Index SpinImage::index::build(std::string quicciImageDumpDirectory, std::string 
             IndexNodeID bucketNodeID = processBucketLink(cache, nextLink, combined.level3.image.data(), 3);
             IndexEntry entry = {fileIndex-1, image};
             cache.insertImageIntoBucketNode(bucketNodeID, entry);
-            bucketNodesPerRootNode.at(combined.level0.image)++;
+            bucketNodesPerRootNode.at(combined.level0.image)++;*/
         }
 
         std::chrono::steady_clock::time_point endTime = std::chrono::steady_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
         std::cout << "\tTook " << float(duration.count()) / 1000.0f << " seconds." << std::endl;
 
-        std::vector<unsigned int> bitCounts = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        /*std::vector<unsigned int> bitCounts = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         std::vector<unsigned int> binCounts = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
         for(int i = 0; i < 65536; i++) {
             bitCounts.at(std::bitset<16>(i).count()) += bucketNodesPerRootNode.at(i);
             binCounts.at(std::bitset<16>(i).count()) ++;
-            /*std::cout << bucketNodesPerRootNode.at(i) << (i == 65535 ? "" : ", ");
-            if(i % 32 == 31) {
-                std::cout << std::endl;
-            }*/
+            //std::cout << bucketNodesPerRootNode.at(i) << (i == 65535 ? "" : ", ");
+            //if(i % 32 == 31) {
+            //    std::cout << std::endl;
+            //}
         }
         for(int i = 0; i < 17; i++) {
             std::cout << bitCounts.at(i) << (i == 16 ? "" : ", ");
@@ -122,12 +149,30 @@ Index SpinImage::index::build(std::string quicciImageDumpDirectory, std::string 
         for(int i = 0; i < 17; i++) {
             std::cout << (float(bitCounts.at(i))/float(binCounts.at(i))) << (i == 16 ? "" : ", ");
         }
-        std::cout << std::endl;
+        std::cout << std::endl;*/
 
         delete[] images.horizontallyIncreasingImages;
         delete[] images.horizontallyDecreasingImages;
     }
 
+    for(int row = 0; row < 64; row++) {
+        for(int col = 0; col < 64; col++) {
+            std::cout << increasingCounts[row * spinImageWidthPixels + col] << (col == 63 ? "" : ", ");
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    for(int row = 0; row < 64; row++) {
+        for(int col = 0; col < 64; col++) {
+            std::cout << decreasingCounts[row * spinImageWidthPixels + col] << (col == 63 ? "" : ", ");
+        }
+        std::cout << std::endl;
+    }
 
 
     // Ensuring all changes are written to disk

ec0785b4c7165582f68a44ef14205956b0e80fdf
bartvbl <bartvblokl@gmail.com>
2019-12-17 16:32:37 +0100

    Some work starting on a better storage system

--- src/spinImage/utilities/fileutils.cpp
+++ src/spinImage/utilities/fileutils.cpp
@@ -9,3 +9,11 @@ std::vector<std::experimental::filesystem::path> SpinImage::utilities::listDirec
 
     return foundFiles;
 }
+
+void SpinImage::utilities::writeCompressedFile(const char *buffer, size_t bufferSize, const std::string &outputArchive, size_t fileID) {
+
+}
+
+void SpinImage::utilities::readCompressedFile(const char *buffer, size_t maxSize, const std::string &archiveFile, size_t fileID) {
+
+}
--- src/spinImage/utilities/fileutils.h
+++ src/spinImage/utilities/fileutils.h
@@ -5,6 +5,8 @@
 namespace SpinImage {
     namespace utilities {
         std::vector<std::experimental::filesystem::path> listDirectory(std::string directory);
+        void writeCompressedFile(const char* buffer, size_t bufferSize, const std::string &archiveFile, size_t fileID);
+        void readCompressedFile(const char* buffer, size_t maxSize, const std::string &archiveFile, size_t fileID);
     }
 }
 

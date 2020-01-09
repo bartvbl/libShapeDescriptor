c532aebb1424d361a5620528a7be2d86fa7b508e
bartvbl <bartvblokl@gmail.com>
2020-01-09 13:40:20 +0100

    Made recompress script a lot faster

--- misc/recompress.py
+++ misc/recompress.py
@@ -2,15 +2,27 @@ import os
 import os.path
 import zipfile
 import lzma
+import subprocess
 
 directory = '/home/bart/Datasets/SHREC2017_QUICCI_images/'
 outdir = '/home/bart/Datasets/SHREC_TEMP/'
 
 allfiles = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f)) and f.endswith('.zip')]
 
+processlist = []
+
 for fileindex, file in enumerate(allfiles):
-	print(fileindex+1, '/', len(allfiles), ':', file, '                         ', end='\r')
-	zip = zipfile.ZipFile(os.path.join(directory, file))
-	imagedata = zip.read('quicci_images.dat')
-	with lzma.open(os.path.join(outdir, file.replace('.zip', '.lz')), "w") as f:
-		f.write(imagedata)
+	if fileindex < 3200:
+		continue
+	print("Started", fileindex+1, '/', len(allfiles), ':', file)
+	infile = os.path.join(directory, file)
+	outfile = os.path.join(outdir, file.replace('.zip', '.lz'))
+	process = subprocess.Popen(['unzip -p "' + infile + '" | p7zip > "' + outfile + '"'], shell=True)
+	processlist.append(process)
+	if fileindex % 10 == 0:
+		for proc in processlist:
+			print('Waiting..')
+			proc.wait()
+		processlist = []
+		
+	

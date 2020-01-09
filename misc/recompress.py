94f8be6f9dce492a854ee828bd98b73889e9ab17
bartvbl <bartvblokl@gmail.com>
2020-01-09 17:30:33 +0100

    Further improved throughput of recompression script

--- misc/recompress.py
+++ misc/recompress.py
@@ -3,6 +3,7 @@ import os.path
 import zipfile
 import lzma
 import subprocess
+import time
 
 directory = '/home/bart/Datasets/SHREC2017_QUICCI_images/'
 outdir = '/home/bart/Datasets/SHREC_TEMP/'
@@ -12,17 +13,20 @@ allfiles = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(dire
 processlist = []
 
 for fileindex, file in enumerate(allfiles):
-	if fileindex < 3200:
+	if fileindex < 6510:
 		continue
 	print("Started", fileindex+1, '/', len(allfiles), ':', file)
 	infile = os.path.join(directory, file)
 	outfile = os.path.join(outdir, file.replace('.zip', '.lz'))
 	process = subprocess.Popen(['unzip -p "' + infile + '" | p7zip > "' + outfile + '"'], shell=True)
 	processlist.append(process)
-	if fileindex % 10 == 0:
+	while len(processlist) == 10:
+		processEnded = False
 		for proc in processlist:
-			print('Waiting..')
-			proc.wait()
-		processlist = []
+			if proc.poll() != None:
+				processlist.remove(proc)
+				processEnded = True
+		if not processEnded:
+			time.sleep(0.1)
 		
 	

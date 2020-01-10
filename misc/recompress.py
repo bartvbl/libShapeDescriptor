0e9d5d2c1307e1ab909d40436f60ea62a07aeea3
bartvbl <bartvblokl@gmail.com>
2020-01-10 16:31:00 +0100

    Reverted recompress script to LZMA2 due to better ratios

--- misc/recompress.py
+++ misc/recompress.py
@@ -6,17 +6,19 @@ import subprocess
 import time
 
 directory = '/home/bart/Datasets/SHREC2017_QUICCI_images/'
-outdir = '/home/bart/Datasets/SHREC_ZSTD/'
+outdir = '/home/bart/Datasets/SHREC_TEMP/'
 
 allfiles = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f)) and f.endswith('.zip')]
 
 processlist = []
 
 for fileindex, file in enumerate(allfiles):
+	if fileindex < 26000:
+		continue
 	print("Started", fileindex+1, '/', len(allfiles), ':', file)
 	infile = os.path.join(directory, file)
 	outfile = os.path.join(outdir, file.replace('.zip', '.lz'))
-	process = subprocess.Popen(['unzip -p "' + infile + '" | zstd -19 -T3 > "' + outfile + '"'], shell=True)
+	process = subprocess.Popen(['unzip -p "' + infile + '" | p7zip > "' + outfile + '"'], shell=True)
 	processlist.append(process)
 	while len(processlist) == 10:
 		processEnded = False

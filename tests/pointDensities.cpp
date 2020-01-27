58afd67ff5b162841091ff62dd32d93e4003f284
Bart van Blokland <bartvblokl@gmail.com>
2020-01-27 19:39:05 +0100

    Added a number of additional test cases

--- tests/pointDensities.cpp
+++ tests/pointDensities.cpp
@@ -41,7 +41,7 @@ TEST_CASE("Counting the number of points in the vicinity of others")
         const float* simpleCloud = new float[12]{
                 -1,  1,  1, -1,
                 -1, -1,  1,  1,
-                0,  0,  0,  0
+                 0,  0,  0,  0
         };
 
         unsigned int *counts = computeNeighbourCounts(simpleCloud, 4, 2);
@@ -56,13 +56,102 @@ TEST_CASE("Counting the number of points in the vicinity of others")
         const float* simpleCloud = new float[12]{
                 -1,  1,  1, -1,
                 -1, -1,  1,  1,
-                0,  0,  0,  0
+                 0,  0,  0,  0
         };
 
         unsigned int *counts = computeNeighbourCounts(simpleCloud, 4, 1);
 
         for(int i = 0; i < 4; i++) {
             REQUIRE(counts[i] == 0);
+        }
+    }
+
+    SECTION("Point cloud with only 1 point") {
+        // note: VERTICAL definitions!
+        const float* simpleCloud = new float[3]{
+                -1,
+                -1,
+                 0
+        };
+
+        unsigned int *counts = computeNeighbourCounts(simpleCloud, 1, 1);
+
+        REQUIRE(counts[0] == 0);
+    }
+
+    SECTION("Two points at the same location") {
+        // note: VERTICAL definitions!
+        const float* simpleCloud = new float[6]{
+                -1, -1,
+                -1, -1,
+                 0,  0
+        };
+
+        unsigned int *counts = computeNeighbourCounts(simpleCloud, 2, 1);
+
+        REQUIRE(counts[0] == 1);
+        REQUIRE(counts[1] == 1);
+    }
+
+    SECTION("Cube point cloud, small radius") {
+        // note: VERTICAL definitions!
+        const float* simpleCloud = new float[24]{
+                -1,  1,  1, -1, -1,  1,  1, -1,
+                -1, -1,  1,  1, -1, -1,  1,  1,
+                -1, -1, -1, -1,  1,  1,  1,  1
+        };
+
+        unsigned int *counts = computeNeighbourCounts(simpleCloud, 8, 1);
+
+        for(int i = 0; i < 8; i++) {
+            REQUIRE(counts[i] == 0);
+        }
+    }
+
+    SECTION("Cube point cloud, medium radius") {
+        // note: VERTICAL definitions!
+        const float* simpleCloud = new float[24]{
+                -1,  1,  1, -1, -1,  1,  1, -1,
+                -1, -1,  1,  1, -1, -1,  1,  1,
+                -1, -1, -1, -1,  1,  1,  1,  1
+        };
+
+        unsigned int *counts = computeNeighbourCounts(simpleCloud, 8, 2);
+
+        for(int i = 0; i < 8; i++) {
+            REQUIRE(counts[i] == 3);
+        }
+    }
+
+    SECTION("Cube point cloud, large radius") {
+        // note: VERTICAL definitions!
+        const float* simpleCloud = new float[24]{
+                -1,  1,  1, -1, -1,  1,  1, -1,
+                -1, -1,  1,  1, -1, -1,  1,  1,
+                -1, -1, -1, -1,  1,  1,  1,  1
+        };
+
+        unsigned int *counts = computeNeighbourCounts(simpleCloud, 8, 5);
+
+        for(int i = 0; i < 8; i++) {
+            REQUIRE(counts[i] == 7);
+        }
+    }
+
+    SECTION("Uneven distribution") {
+        // note: VERTICAL definitions!
+        const float* simpleCloud = new float[24]{
+                -5, -4, -2, -1,  0,  3,  4,  8,
+                 1,  1,  1,  1,  1,  1,  1,  1,
+                 0,  0,  0,  0,  0,  0,  0,  0
+        };
+
+        unsigned int *counts = computeNeighbourCounts(simpleCloud, 8, 1);
+
+        unsigned int correctCounts[] = {1, 1, 1, 2, 1, 1, 1, 0};
+
+        for(int i = 0; i < 8; i++) {
+            REQUIRE(counts[i] == correctCounts[i]);
         }
     }
 }

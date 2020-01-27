ea74ac40990cf43f3826a776df155b63222ee032
bartvbl <bartvblokl@gmail.com>
2020-01-27 16:10:03 +0100

    Added test for point density computations

---
+++ tests/pointDensities.cpp
@@ -0,0 +1,31 @@
+#include <catch2/catch.hpp>
+#include <cuda_runtime.h>
+#include <vector_types.h>
+#include <spinImage/gpu/types/PointCloud.h>
+#include <spinImage/common/types/array.h>
+#include <spinImage/utilities/pointCloudUtils.h>
+
+TEST_CASE("Counting the number of points in the vicinity of others") {
+    SECTION("Simple point cloud") {
+        // note: VERTICAL definitions!
+        const float* simpleCloud = new float[12]{
+            -1,  1,  1, -1,
+            -1, -1,  1,  1,
+             0,  0,  0,  0
+        };
+
+        SpinImage::gpu::PointCloud device_pointCloud(4);
+
+        cudaMemcpy(device_pointCloud.vertices.array, simpleCloud, 12 * sizeof(float), cudaMemcpyHostToDevice);
+
+        SpinImage::array<unsigned int> device_pointDensities = SpinImage::utilities::computePointDensities(5, device_pointCloud);
+
+        unsigned int* counts = new unsigned int[4];
+        cudaMemcpy(counts, device_pointDensities.content, 4 * sizeof(unsigned int), cudaMemcpyDeviceToHost);
+
+        for(int i = 0; i < 4; i++) {
+            REQUIRE(counts[i] == 3);
+        }
+
+    }
+}

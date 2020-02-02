ab174dcabd1c8ae140496f3e706f6d9bb47ed1be
Bart van Blokland <bartvblokl@gmail.com>
2020-02-02 15:22:39 +0100

    Finished implementing OBJ highlight export, except from MTL lib path

--- src/spinImage/utilities/dumpers/meshDumper.cpp
+++ src/spinImage/utilities/dumpers/meshDumper.cpp
@@ -5,14 +5,30 @@
 
 void dumpMesh(SpinImage::cpu::Mesh mesh, std::string &outputFilePath, std::vector<SpinImage::cpu::float2> &textureCoordinates) {
 
-    std::ofstream materialFile;
+    bool hasHighlightsEnabled = textureCoordinates.size() > 0;
 
+    if(hasHighlightsEnabled) {
+        std::ofstream materialFile;
+        materialFile.open();
+
+        materialFile << "newmtl highlightMaterial" << std::endl;
+        materialFile << "Ns 10.0000" << std::endl;
+        materialFile << "Ni 1.5000" << std::endl;
+        materialFile << "d 1.0000" << std::endl;
+        materialFile << "Tr 0.0000" << std::endl;
+        materialFile << "Tf 1.0000 1.0000 1.0000" << std::endl;
+        materialFile << "illum 2" << std::endl;
+        materialFile << "Ka 0.0000 0.0000 0.0000" << std::endl;
+        materialFile << "Kd 1.0000 1.0000 1.0000" << std::endl;
+        materialFile << "Ks 1.0000 1.0000 1.0000" << std::endl;
+        materialFile << "Ke 0.0000 0.0000 0.0000" << std::endl;
+    }
 
     std::ofstream outputFile;
     outputFile.open(outputFilePath);
 
     outputFile << "mtlllib exportedObject.mtl" << std::endl;
-    
+
     outputFile << std::endl;
 
     for(unsigned int i = 0; i < mesh.vertexCount; i++) {
@@ -21,11 +37,13 @@ void dumpMesh(SpinImage::cpu::Mesh mesh, std::string &outputFilePath, std::vecto
 
     outputFile << std::endl;
 
-    for(unsigned int i = 0; i < mesh.vertexCount; i++) {
-        outputFile << "vt " << textureCoordinates[i].x << " " << textureCoordinates[i].y << std::endl;
-    }
+    if(hasHighlightsEnabled) {
+        for(unsigned int i = 0; i < mesh.vertexCount; i++) {
+            outputFile << "vt " << textureCoordinates[i].x << " " << textureCoordinates[i].y << std::endl;
+        }
 
-    outputFile << std::endl;
+        outputFile << std::endl;
+    }
 
     for(unsigned int i = 0; i < mesh.vertexCount; i++) {
         outputFile << "vn " << mesh.normals[i].x << " " << mesh.normals[i].y << " " <<mesh.normals[i].z << std::endl;
@@ -34,10 +52,25 @@ void dumpMesh(SpinImage::cpu::Mesh mesh, std::string &outputFilePath, std::vecto
     outputFile << std::endl;
     outputFile << "o object" << std::endl;
     outputFile << "g object" << std::endl;
+
+    if(hasHighlightsEnabled) {
+        outputFile << "usemtl highlightMaterial" << std::endl;
+    }
+
     outputFile << std::endl;
 
     for(unsigned int i = 0; i < mesh.vertexCount; i += 3) {
-        outputFile << "f " << (i+1) << "//" << (i+1) << " " << (i+2) << "//" << (i+2) << " " << (i+3) << "//" << (i+3) << std::endl;
+        if(hasHighlightsEnabled) {
+            outputFile << "f "
+                       << (i+1) << "//" << (i+1) << " "
+                       << (i+2) << "//" << (i+2) << " "
+                       << (i+3) << "//" << (i+3) << std::endl;
+        } else {
+            outputFile << "f "
+                       << (i+1) << "/" << (i+1) << "/" << (i+1) << " "
+                       << (i+2) << "/" << (i+2) << "/" << (i+2) << " "
+                       << (i+3) << "/" << (i+3) << "/" << (i+3) << std::endl;
+        }
     }
 
     outputFile.close();

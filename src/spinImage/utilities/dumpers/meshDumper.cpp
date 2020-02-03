3c84f15feecb2d8b061c14720facd8c2e0dd143e
bartvbl <bartvblokl@gmail.com>
2020-02-03 15:54:00 +0100

    Highlighted OBJ export is now working properly

--- src/spinImage/utilities/dumpers/meshDumper.cpp
+++ src/spinImage/utilities/dumpers/meshDumper.cpp
@@ -3,13 +3,33 @@
 #include <iostream>
 #include <vector>
 
-void dumpMesh(SpinImage::cpu::Mesh mesh, std::string &outputFilePath, std::vector<SpinImage::cpu::float2> &textureCoordinates) {
+void dumpMesh(SpinImage::cpu::Mesh mesh, std::experimental::filesystem::path &outputFilePath, size_t highlightStartVertex, size_t highlightEndVertex) {
 
-    bool hasHighlightsEnabled = textureCoordinates.size() > 0;
+    bool hasHighlightsEnabled =
+            (highlightStartVertex > 0 && highlightStartVertex < mesh.vertexCount) ||
+            (highlightEndVertex > 0 && highlightEndVertex < mesh.vertexCount);
+
+    std::ofstream outputFile;
+    outputFile.open(outputFilePath);
 
     if(hasHighlightsEnabled) {
+        std::experimental::filesystem::path materialLibPath =
+                outputFilePath.parent_path() / "exportedObject.mtl";
+
         std::ofstream materialFile;
-        materialFile.open();
+        materialFile.open(materialLibPath.string());
+
+        materialFile << "newmtl regularMaterial" << std::endl;
+        materialFile << "Ns 10.0000" << std::endl;
+        materialFile << "Ni 1.5000" << std::endl;
+        materialFile << "d 1.0000" << std::endl;
+        materialFile << "Tr 0.0000" << std::endl;
+        materialFile << "Tf 1.0000 1.0000 1.0000" << std::endl;
+        materialFile << "illum 2" << std::endl;
+        materialFile << "Ka 0.0000 0.0000 0.0000" << std::endl;
+        materialFile << "Kd 0.8000 0.8000 0.8000" << std::endl;
+        materialFile << "Ks 0.8000 0.8000 0.8000" << std::endl;
+        materialFile << "Ke 0.0000 0.0000 0.0000" << std::endl << std::endl;
 
         materialFile << "newmtl highlightMaterial" << std::endl;
         materialFile << "Ns 10.0000" << std::endl;
@@ -19,17 +39,16 @@ void dumpMesh(SpinImage::cpu::Mesh mesh, std::string &outputFilePath, std::vecto
         materialFile << "Tf 1.0000 1.0000 1.0000" << std::endl;
         materialFile << "illum 2" << std::endl;
         materialFile << "Ka 0.0000 0.0000 0.0000" << std::endl;
-        materialFile << "Kd 1.0000 1.0000 1.0000" << std::endl;
-        materialFile << "Ks 1.0000 1.0000 1.0000" << std::endl;
+        materialFile << "Kd 0.6000 0.0000 0.0000" << std::endl;
+        materialFile << "Ks 0.8000 0.8000 0.8000" << std::endl;
         materialFile << "Ke 0.0000 0.0000 0.0000" << std::endl;
+
+        materialFile.close();
+
+        outputFile << "mtllib exportedObject.mtl" << std::endl;
+
+        outputFile << std::endl;
     }
-
-    std::ofstream outputFile;
-    outputFile.open(outputFilePath);
-
-    outputFile << "mtlllib exportedObject.mtl" << std::endl;
-
-    outputFile << std::endl;
 
     for(unsigned int i = 0; i < mesh.vertexCount; i++) {
         outputFile << "v " << mesh.vertices[i].x << " " << mesh.vertices[i].y << " " <<mesh.vertices[i].z << std::endl;
@@ -37,65 +56,48 @@ void dumpMesh(SpinImage::cpu::Mesh mesh, std::string &outputFilePath, std::vecto
 
     outputFile << std::endl;
 
-    if(hasHighlightsEnabled) {
-        for(unsigned int i = 0; i < mesh.vertexCount; i++) {
-            outputFile << "vt " << textureCoordinates[i].x << " " << textureCoordinates[i].y << std::endl;
-        }
-
-        outputFile << std::endl;
-    }
-
     for(unsigned int i = 0; i < mesh.vertexCount; i++) {
         outputFile << "vn " << mesh.normals[i].x << " " << mesh.normals[i].y << " " <<mesh.normals[i].z << std::endl;
     }
 
-    outputFile << std::endl;
-    outputFile << "o object" << std::endl;
-    outputFile << "g object" << std::endl;
-
-    if(hasHighlightsEnabled) {
-        outputFile << "usemtl highlightMaterial" << std::endl;
-    }
+    bool lastIterationWasHighlighted = false;
 
     outputFile << std::endl;
 
     for(unsigned int i = 0; i < mesh.vertexCount; i += 3) {
-        if(hasHighlightsEnabled) {
-            outputFile << "f "
-                       << (i+1) << "//" << (i+1) << " "
-                       << (i+2) << "//" << (i+2) << " "
-                       << (i+3) << "//" << (i+3) << std::endl;
-        } else {
-            outputFile << "f "
-                       << (i+1) << "/" << (i+1) << "/" << (i+1) << " "
-                       << (i+2) << "/" << (i+2) << "/" << (i+2) << " "
-                       << (i+3) << "/" << (i+3) << "/" << (i+3) << std::endl;
+        bool currentIterationIsHighlighted = i >= highlightStartVertex && i < highlightEndVertex;
+
+        if(currentIterationIsHighlighted != lastIterationWasHighlighted || i == 0) {
+            outputFile << std::endl;
+            outputFile << "o object_" << i << std::endl;
+            outputFile << "g object_" << i << std::endl;
+
+            if(currentIterationIsHighlighted) {
+                outputFile << "usemtl highlightMaterial" << std::endl;
+            } else {
+                outputFile << "usemtl regularMaterial" << std::endl;
+            }
+            outputFile << std::endl;
         }
+
+        outputFile << "f "
+           << (i+1) << "//" << (i+1) << " "
+           << (i+2) << "//" << (i+2) << " "
+           << (i+3) << "//" << (i+3) << std::endl;
+        lastIterationWasHighlighted = currentIterationIsHighlighted;
     }
 
     outputFile.close();
 }
 
-void SpinImage::dump::mesh(cpu::Mesh mesh, std::string outputFilePath) {
-    std::vector<SpinImage::cpu::float2> empty;
-    dumpMesh(mesh, outputFilePath, empty);
+void SpinImage::dump::mesh(cpu::Mesh mesh, std::experimental::filesystem::path outputFilePath) {
+    // Highlight a range that will never be highlighted
+    dumpMesh(mesh, outputFilePath, -1, -1);
 }
 
-void SpinImage::dump::mesh(cpu::Mesh mesh, std::string outputFilePath,
+void SpinImage::dump::mesh(cpu::Mesh mesh, std::experimental::filesystem::path outputFilePath,
         size_t highlightStartVertex, size_t highlightEndVertex) {
-    std::vector<SpinImage::cpu::float2> textureCoordinates;
-    textureCoordinates.resize(mesh.vertexCount);
 
-    for(size_t i = 0; i < mesh.vertexCount; i++) {
-        if(i >= highlightStartVertex && i < highlightEndVertex) {
-            textureCoordinates.at(i).x = 1;
-            textureCoordinates.at(i).y = 1;
-        } else {
-            textureCoordinates.at(i).x = 0;
-            textureCoordinates.at(i).y = 0;
-        }
-    }
-
-    dumpMesh(mesh, outputFilePath, textureCoordinates);
+    dumpMesh(mesh, outputFilePath, highlightStartVertex, highlightEndVertex);
 }
 
--- src/spinImage/utilities/dumpers/meshDumper.h
+++ src/spinImage/utilities/dumpers/meshDumper.h
@@ -1,11 +1,12 @@
 #pragma once
 
 #include <spinImage/cpu/types/Mesh.h>
+#include <experimental/filesystem>
 
 namespace SpinImage {
     namespace dump {
-        void mesh(cpu::Mesh mesh, std::string outputFile);
-        void mesh(cpu::Mesh mesh, std::string outputFilePath,
+        void mesh(cpu::Mesh mesh, std::experimental::filesystem::path outputFile);
+        void mesh(cpu::Mesh mesh, std::experimental::filesystem::path outputFilePath,
                 size_t highlightStartVertex, size_t highlightEndVertex);
     }
 }

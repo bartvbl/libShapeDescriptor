9135519a74719c8ff4c5008d7348870baeb1ec6b
bartvbl <bartvblokl@gmail.com>
2020-03-06 16:59:27 +0100

    Final change before firing off the index build

--- src/spinImage/cpu/index/IndexBuilder.cpp
+++ src/spinImage/cpu/index/IndexBuilder.cpp
@@ -153,7 +153,7 @@ Index SpinImage::index::build(
 
     NodeBlock* rootBlock = new NodeBlock();
 
-    const size_t cacheNodeBlockCapacity = 50000;
+    const size_t cacheNodeBlockCapacity = 75000;
     const size_t cacheImageCapacity = 75000000;
     NodeBlockCache cache(cacheNodeBlockCapacity, cacheImageCapacity, indexDirectory, rootBlock);
 

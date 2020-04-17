bd05de7bd88a5bf68b1a7be580dd5eec255901c1
Bart van Blokland <bartvblokl@gmail.com>
2020-04-17 18:27:22 +0200

    Implementation of list sorting phase

--- src/spinImage/cpu/index/listConstructor/ListSorter.h
+++ src/spinImage/cpu/index/listConstructor/ListSorter.h
@@ -2,8 +2,4 @@
 
 #include <experimental/filesystem>
 
-void sortListFiles(
-        const std::experimental::filesystem::path &quicciImageDumpDirectory,
-        std::experimental::filesystem::path &indexDumpDirectory,
-        size_t openFileLimit,
-        size_t fileStartIndex, size_t fileEndIndex);
+void sortListFiles(std::experimental::filesystem::path &indexDumpDirectory);
--- src/spinImage/cpu/index/types/IndexEntry.h
+++ src/spinImage/cpu/index/types/IndexEntry.h
@@ -2,6 +2,8 @@
 
 #include <cstddef>
 #include <spinImage/cpu/index/types/Index.h>
+#include <cassert>
+#include <iostream>
 
 struct IndexEntry {
     // To save space, we only store the index of the file where the entry originated from.
@@ -21,3 +23,18 @@ struct IndexEntry {
     // Default constructor to allow std::vector resizing
     IndexEntry() : fileIndex(0), imageIndex(0), remainingPixelCount(0) {}
 };
+
+bool indexEntryComparator(const IndexEntry& lhs, const IndexEntry& rhs) {
+    if(lhs.remainingPixelCount != rhs.remainingPixelCount) {
+        return lhs.remainingPixelCount < rhs.remainingPixelCount;
+    }
+
+    if(lhs.fileIndex != rhs.fileIndex) {
+        return lhs.fileIndex < rhs.fileIndex;
+    }
+
+    if(lhs.imageIndex != rhs.imageIndex) {
+        return lhs.imageIndex < rhs.imageIndex;
+    }
+    return false;
+}

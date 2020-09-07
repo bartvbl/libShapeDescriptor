b7ba7826f7b0b42e1c8042aafccb2f35c2f5a9cf
bartvbl <bartvblokl@gmail.com>
2020-09-07 21:09:02 +0200

    Added an OrientedPoint struct intended to be used on the CPU side

---
+++ src/shapeDescriptor/cpu/types/OrientedPoint.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <vector_types.h>
+
+namespace ShapeDescriptor {
+    namespace cpu {
+        struct OrientedPoint {
+            float3 vertex;
+            float3 normal;
+        };
+    }
+}
+

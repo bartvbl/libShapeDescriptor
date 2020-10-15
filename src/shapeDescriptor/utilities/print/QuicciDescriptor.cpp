e465ca72cd82eddc5efc0c9cab7d8eb51bd9503d
bartvbl <bartvblokl@gmail.com>
2020-10-15 14:24:54 +0200

    Added a function for visualising QUICCI descriptors

---
+++ src/shapeDescriptor/utilities/print/QuicciDescriptor.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "QuicciDescriptor.h"
+
+void ShapeDescriptor::print::quicciDescriptor(ShapeDescriptor::QUICCIDescriptor &descriptor) {
+    std::cout << "+";
+    for(unsigned int col = 0; col < spinImageWidthPixels; col++) {
+        std::cout << "-";
+    }
+    std::cout << "+" << std::endl;
+
+    for(unsigned int row = 0; row < spinImageWidthPixels; row++) {
+        std::cout << '|';
+        for(unsigned int col = 0; col < spinImageWidthPixels; col++) {
+            unsigned int chunkIndex = (row * spinImageWidthPixels) / 32;
+            std::cout << (((descriptor.contents[chunkIndex] & (0x80000000U >> (col % 32U))) != 0) ? 'X' : ' ');
+        }
+        std::cout << '|' << std::endl;
+    }
+
+    std::cout << "+";
+    for(unsigned int col = 0; col < spinImageWidthPixels; col++) {
+        std::cout << "-";
+    }
+    std::cout << "+" << std::endl;
+}
---
+++ src/shapeDescriptor/utilities/print/QuicciDescriptor.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <shapeDescriptor/common/types/methods/QUICCIDescriptor.h>
+
+namespace ShapeDescriptor {
+    namespace print {
+        void quicciDescriptor(ShapeDescriptor::QUICCIDescriptor &descriptor);
+    }
+}

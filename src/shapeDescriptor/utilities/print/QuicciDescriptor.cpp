8591ef38958261ed121bff6761209f9749f56f1e
bartvbl <bartvblokl@gmail.com>
2020-10-15 15:56:37 +0200

    Fixed not all bits appearing when printing QUICCI descriptors

--- src/shapeDescriptor/utilities/print/QuicciDescriptor.cpp
+++ src/shapeDescriptor/utilities/print/QuicciDescriptor.cpp
@@ -11,7 +11,7 @@ void ShapeDescriptor::print::quicciDescriptor(ShapeDescriptor::QUICCIDescriptor 
     for(unsigned int row = 0; row < spinImageWidthPixels; row++) {
         std::cout << '|';
         for(unsigned int col = 0; col < spinImageWidthPixels; col++) {
-            unsigned int chunkIndex = (row * spinImageWidthPixels) / 32;
+            unsigned int chunkIndex = (row * spinImageWidthPixels + col) / (8 * sizeof(unsigned int));
             std::cout << (((descriptor.contents[chunkIndex] & (0x80000000U >> (col % 32U))) != 0) ? 'X' : ' ');
         }
         std::cout << '|' << std::endl;

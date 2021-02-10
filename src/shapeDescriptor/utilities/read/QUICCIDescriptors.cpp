d559ff569d5844d4bad2119eafe8ec9bdd31898e
bartvbl <bartvblokl@gmail.com>
2021-02-10 16:12:22 +0100

    Added a function to read out image count from dump files

--- src/shapeDescriptor/utilities/read/QUICCIDescriptors.cpp
+++ src/shapeDescriptor/utilities/read/QUICCIDescriptors.cpp
@@ -6,20 +6,31 @@
 #include <shapeDescriptor/common/types/methods/QUICCIDescriptor.h>
 #include <shapeDescriptor/cpu/types/array.h>
 
+ShapeDescriptor::QUICCIDescriptorFileHeader readHeader(const char* startOfFileBuffer) {
+    char fileID[5] = {startOfFileBuffer[0], startOfFileBuffer[1], startOfFileBuffer[2], startOfFileBuffer[3], '\0'};
+    if(std::string(fileID) != "QUIC") {
+        std::cout << "WARNING: File header does not match expectations, and is thus possibly corrupt." << std::endl;
+    }
+
+    size_t imageCount = *reinterpret_cast<const size_t*>(startOfFileBuffer + 5);
+    unsigned int descriptorWidthPixels = *reinterpret_cast<const unsigned int*>(startOfFileBuffer + 5 + sizeof(size_t));
+
+    ShapeDescriptor::QUICCIDescriptorFileHeader header;
+    header.fileID = std::string(fileID);
+    header.imageCount = imageCount;
+    header.descriptorWidthPixels = descriptorWidthPixels;
+
+    return header;
+}
+
 ShapeDescriptor::cpu::array<ShapeDescriptor::QUICCIDescriptor> readImageLZFile(const std::experimental::filesystem::path &path, unsigned int decompressionThreadCount) {
     size_t bufferSize;
     const char* inputBuffer = ShapeDescriptor::utilities::readCompressedFile(path, &bufferSize, decompressionThreadCount);
 
-    char header[5] = {inputBuffer[0], inputBuffer[1], inputBuffer[2], inputBuffer[3], '\0'};
-    if(std::string(header) != "QUIC") {
-        std::cout << "WARNING: File header does not match expectations, and is thus possibly corrupt." << std::endl;
-    }
-
-    size_t imageCount = *reinterpret_cast<const size_t*>(inputBuffer + 5);
-    unsigned int descriptorWidthPixels = *reinterpret_cast<const unsigned int*>(inputBuffer + 5 + sizeof(size_t));
+    ShapeDescriptor::QUICCIDescriptorFileHeader header = readHeader(inputBuffer);
 
     //std::cout << "\tFile has " << imageCount << " images" << std::endl;
-    if(descriptorWidthPixels != spinImageWidthPixels) {
+    if(header.descriptorWidthPixels != spinImageWidthPixels) {
         std::cout << "The libSpinImage library was compiled with a different image size compared to those stored in this file." << std::endl;
         std::cout << "This means any processing this program does on them will not work correctly." << std::endl;
         std::cout << "Aborting index construction.." << std::endl;
@@ -27,13 +38,13 @@ ShapeDescriptor::cpu::array<ShapeDescriptor::QUICCIDescriptor> readImageLZFile(c
     }
 
     ShapeDescriptor::cpu::array<ShapeDescriptor::QUICCIDescriptor> images;
-    images.length = imageCount;
-    images.content = new ShapeDescriptor::QUICCIDescriptor[imageCount];
+    images.length = header.imageCount;
+    images.content = new ShapeDescriptor::QUICCIDescriptor[header.imageCount];
 
     const ShapeDescriptor::QUICCIDescriptor* imagesBasePointer
-        = reinterpret_cast<const ShapeDescriptor::QUICCIDescriptor*>(inputBuffer + 5 + sizeof(size_t) + sizeof(unsigned int));
+        = reinterpret_cast<const ShapeDescriptor::QUICCIDescriptor*>(inputBuffer + sizeof(ShapeDescriptor::QUICCIDescriptorFileHeader));
 
-    std::copy(imagesBasePointer, imagesBasePointer + imageCount, images.content);
+    std::copy(imagesBasePointer, imagesBasePointer + header.imageCount, images.content);
 
     delete[] inputBuffer;
     return images;
@@ -41,6 +52,19 @@ ShapeDescriptor::cpu::array<ShapeDescriptor::QUICCIDescriptor> readImageLZFile(c
 
 ShapeDescriptor::cpu::array<ShapeDescriptor::QUICCIDescriptor> ShapeDescriptor::read::QUICCIDescriptors(const std::experimental::filesystem::path &dumpFileLocation, unsigned int decompressionThreadCount) {
     return readImageLZFile(dumpFileLocation, decompressionThreadCount);
+}
+
+ShapeDescriptor::QUICCIDescriptorFileHeader
+ShapeDescriptor::read::QuicciDescriptorFileHeader(const std::experimental::filesystem::path &path) {
+    size_t bufferSize;
+    const size_t headerSize = sizeof(ShapeDescriptor::QUICCIDescriptorFileHeader);
+    const char* inputBuffer = ShapeDescriptor::utilities::readCompressedFileUpToNBytes(path, &bufferSize, headerSize, 1);
+
+    ShapeDescriptor::QUICCIDescriptorFileHeader header = readHeader(inputBuffer);
+
+    delete[] inputBuffer;
+
+    return header;
 }
 
 
--- src/shapeDescriptor/utilities/read/QUICCIDescriptors.h
+++ src/shapeDescriptor/utilities/read/QUICCIDescriptors.h
@@ -6,8 +6,15 @@
 #include <shapeDescriptor/cpu/types/array.h>
 
 namespace ShapeDescriptor {
+    struct QUICCIDescriptorFileHeader {
+        std::string fileID;
+        size_t imageCount;
+        unsigned int descriptorWidthPixels;
+    };
+
     namespace read {
         ShapeDescriptor::cpu::array<ShapeDescriptor::QUICCIDescriptor> QUICCIDescriptors(const std::experimental::filesystem::path &dumpFileLocation, unsigned int decompressionThreadCount = 1);
+        QUICCIDescriptorFileHeader QuicciDescriptorFileHeader(const std::experimental::filesystem::path &dumpFileLocation);
     }
 }
 

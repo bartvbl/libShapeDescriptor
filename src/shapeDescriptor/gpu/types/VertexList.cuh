c2d0879ec7a86de087a61f7ef96372f4bd18e5c1
Bart van Blokland <bartvblokl@gmail.com>
2021-03-06 23:41:48 +0100

    Revert "hopefully array.h now compiles when nvcc is not available"
    This reverts commit 86dbe492b99490e5c313ff3fb9a6069a1f75f896.

--- src/shapeDescriptor/gpu/types/VertexList.cuh
+++ src/shapeDescriptor/gpu/types/VertexList.cuh
@@ -1,33 +1,26 @@
 #pragma once
 
 #include <cassert>
-#ifdef CUDACC
+
 #include <cuda_runtime_api.h>
 #include <device_launch_parameters.h>
 #include <nvidia/helper_cuda.h>
-#else
-    struct float3 {
-    float x; float y; float z;
-    };
-#endif
+
 namespace ShapeDescriptor {
     namespace gpu {
         struct VertexList {
             float* array;
             size_t length;
-#ifdef CUDACC
+
             VertexList(size_t length) {
                 checkCudaErrors(cudaMalloc((void**) &array, 3 * length * sizeof(float)));
                 this->length = length;
             }
-#endif
 
             // For copying
             VertexList() {}
-#ifdef CUDACC
-            __device__
-#endif
-            float3 at(size_t index) {
+
+            __device__ float3 at(size_t index) {
                 assert(index < length);
 
                 float3 item;
@@ -37,21 +30,17 @@ namespace ShapeDescriptor {
                 return item;
             }
 
-#ifdef CUDACC
-            __device__
-#endif
-            void set(size_t index, float3 value) {
+            __device__ void set(size_t index, float3 value) {
                 assert(index < length);
 
                 array[index] = value.x;
                 array[index + length] = value.y;
                 array[index + 2 * length] = value.z;
             }
-#ifdef CUDACC
+
             void free() {
                 checkCudaErrors(cudaFree(array));
             }
-#endif
         };
     }
 }
--- src/shapeDescriptor/gpu/types/array.h
+++ src/shapeDescriptor/gpu/types/array.h
@@ -1,9 +1,7 @@
 #pragma once
 
 #include <cstddef>
-#ifdef CUDACC
 #include <cuda_runtime.h>
-#endif
 
 namespace ShapeDescriptor {
     namespace gpu {
@@ -11,24 +9,17 @@ namespace ShapeDescriptor {
         {
             size_t length;
             TYPE* content;
-#ifdef CUDACC
-            __host__ __device__
-#endif
-            array() {}
 
-#ifdef CUDACC
-            __host__
-#endif
-            array(size_t length) {
+            __host__ __device__ array() {}
+
+            __host__ array(size_t length) {
                 this->length = length;
                 cudaMalloc(&content, length * sizeof(TYPE));
             }
-#ifdef CUDACC
-            __host__ __device__
-#endif
-            array(size_t length, TYPE* content)
-                    : length(length),
-                      content(content) {}
+
+            __host__ __device__ array(size_t length, TYPE* content)
+                : length(length),
+                  content(content) {}
         };
     }
 }

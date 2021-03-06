86dbe492b99490e5c313ff3fb9a6069a1f75f896
Bart van Blokland <bartvblokl@gmail.com>
2021-03-06 23:33:29 +0100

    hopefully array.h now compiles when nvcc is not available

--- src/shapeDescriptor/gpu/types/array.h
+++ src/shapeDescriptor/gpu/types/array.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <cstddef>
+#ifdef CUDACC
 #include <cuda_runtime.h>
+#endif
 
 namespace ShapeDescriptor {
     namespace gpu {
@@ -9,17 +11,24 @@ namespace ShapeDescriptor {
         {
             size_t length;
             TYPE* content;
+#ifdef CUDACC
+            __host__ __device__
+#endif
+            array() {}
 
-            __host__ __device__ array() {}
-
-            __host__ array(size_t length) {
+#ifdef CUDACC
+            __host__
+#endif
+            array(size_t length) {
                 this->length = length;
                 cudaMalloc(&content, length * sizeof(TYPE));
             }
-
-            __host__ __device__ array(size_t length, TYPE* content)
-                : length(length),
-                  content(content) {}
+#ifdef CUDACC
+            __host__ __device__
+#endif
+            array(size_t length, TYPE* content)
+                    : length(length),
+                      content(content) {}
         };
     }
 }

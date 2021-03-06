a5b86eae519d32bf41ba3c3448e0e82eee11aaa5
Bart van Blokland <bartvblokl@gmail.com>
2021-03-06 23:38:33 +0100

    Possibly made nvidia's docker setup happy

--- src/shapeDescriptor/gpu/types/VertexList.cuh
+++ src/shapeDescriptor/gpu/types/VertexList.cuh
@@ -1,26 +1,33 @@
 #pragma once
 
 #include <cassert>
-
+#ifdef CUDACC
 #include <cuda_runtime_api.h>
 #include <device_launch_parameters.h>
 #include <nvidia/helper_cuda.h>
-
+#else
+    struct float3 {
+    float x; float y; float z;
+    };
+#endif
 namespace ShapeDescriptor {
     namespace gpu {
         struct VertexList {
             float* array;
             size_t length;
-
+#ifdef CUDACC
             VertexList(size_t length) {
                 checkCudaErrors(cudaMalloc((void**) &array, 3 * length * sizeof(float)));
                 this->length = length;
             }
+#endif
 
             // For copying
             VertexList() {}
-
-            __device__ float3 at(size_t index) {
+#ifdef CUDACC
+            __device__
+#endif
+            float3 at(size_t index) {
                 assert(index < length);
 
                 float3 item;
@@ -30,17 +37,21 @@ namespace ShapeDescriptor {
                 return item;
             }
 
-            __device__ void set(size_t index, float3 value) {
+#ifdef CUDACC
+            __device__
+#endif
+            void set(size_t index, float3 value) {
                 assert(index < length);
 
                 array[index] = value.x;
                 array[index + length] = value.y;
                 array[index + 2 * length] = value.z;
             }
-
+#ifdef CUDACC
             void free() {
                 checkCudaErrors(cudaFree(array));
             }
+#endif
         };
     }
 }

73b1be3055596eb2e1f2f415e749d5a34cc26717
bartvbl <bartvblokl@gmail.com>
2023-09-08 16:31:41 +0200

    Compilation fix (collision with the cmath header)

--- lib/nvidia-samples-common/nvidia/helper_math.h
+++ lib/nvidia-samples-common/nvidia/helper_math.h
@@ -1143,10 +1143,6 @@ inline __host__ __device__ uint4 max(uint4 a, uint4 b)
 // - linear interpolation between a and b, based on value t in [0, 1] range
 ////////////////////////////////////////////////////////////////////////////////
 
-inline __device__ __host__ float lerp(float a, float b, float t)
-{
-    return a + t*(b-a);
-}
 inline __device__ __host__ float2 lerp(float2 a, float2 b, float t)
 {
     return a + t*(b-a);

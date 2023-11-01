6006b0d8e7eeb949b1e48f337a0ba191343064b1
bartvbl <bartvblokl@gmail.com>
2023-11-01 15:18:50 +0100

    Removed double definition

--- src/shapeDescriptor/utilities/meshSampler.cpp
+++ src/shapeDescriptor/utilities/meshSampler.cpp
@@ -1,9 +1,5 @@
 #include <shapeDescriptor/shapeDescriptor.h>
 
-ShapeDescriptor::gpu::PointCloud ShapeDescriptor::sampleMesh(ShapeDescriptor::gpu::Mesh mesh, size_t sampleCount, size_t randomSamplingSeed, internal::MeshSamplingBuffers* keepComputedBuffersForExternalUse) {
-    return ShapeDescriptor::sampleMesh(mesh, sampleCount, randomSamplingSeed);
-}
-
 ShapeDescriptor::cpu::PointCloud ShapeDescriptor::sampleMesh(ShapeDescriptor::cpu::Mesh mesh, size_t sampleCount, size_t randomSamplingSeed) {
     ShapeDescriptor::gpu::Mesh device_mesh = mesh.copyToGPU();
     ShapeDescriptor::gpu::PointCloud device_pointCloud = sampleMesh(device_mesh, sampleCount, randomSamplingSeed);

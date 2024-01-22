19bed29fdb4f4c85ab950b750a4910b30cd494d2
bartvbl (TURBONINJA) <bartvblokl@gmail.com>
2024-01-22 22:41:18 +0100

    Added function for reading descriptor files

--- include/shapeDescriptor/compressedDescriptorIO.h
+++ include/shapeDescriptor/compressedDescriptorIO.h
@@ -9,10 +9,9 @@ namespace ShapeDescriptor {
             const std::filesystem::path &outputDumpFile,
             const ShapeDescriptor::cpu::array<DescriptorType> &images,
             unsigned int compressionThreadCount) {
-        const unsigned int imageWidthPixels = spinImageWidthPixels;
 
         size_t imageBlockSize = images.length * sizeof(DescriptorType);
-        size_t outFileBufferSize = 5 + sizeof(size_t) + sizeof(unsigned int) + 2 * imageBlockSize;
+        size_t outFileBufferSize = 5 + sizeof(size_t) + 2 * imageBlockSize;
         char *outFileBuffer = new char[outFileBufferSize];
 
         const std::string header = "CSDF";
@@ -20,14 +19,34 @@ namespace ShapeDescriptor {
         std::copy(header.begin(), header.end(), outFileBuffer);
         outFileBuffer[4] = 0;
         *reinterpret_cast<size_t *>(outFileBuffer + 5) = images.length;
-        *reinterpret_cast<unsigned int *>(outFileBuffer + 5 + sizeof(size_t)) = imageWidthPixels;
         std::copy(images.content, images.content + images.length,
-                  reinterpret_cast<DescriptorType*>(outFileBuffer + 5 + sizeof(size_t) +
-                                                                        sizeof(unsigned int)));
+                  reinterpret_cast<DescriptorType*>(outFileBuffer + 5 + sizeof(size_t)));
 
         ShapeDescriptor::writeCompressedFile(outFileBuffer, outFileBufferSize, outputDumpFile, compressionThreadCount);
-
         delete[] outFileBuffer;
+    }
+
+    template<typename DescriptorType>
+    ShapeDescriptor::cpu::array<DescriptorType> readCompressedDescriptors(
+            const std::filesystem::path &compressedFileLocation, unsigned int decompressionThreadCount = 1) {
+        std::vector<char> inputBuffer = ShapeDescriptor::readCompressedFile(compressedFileLocation, decompressionThreadCount);
+
+        std::array<char, 5> fileID {0, 0, 0, 0, 0};
+        fileID = {inputBuffer.at(0), inputBuffer.at(1), inputBuffer.at(2), inputBuffer.at(3)};
+        if(std::string(fileID.data()) != "CSDF") {
+            std::cout << "WARNING: File header does not match expectations, and is thus possibly corrupt." << std::endl;
+        }
+
+        size_t imageCount = *reinterpret_cast<const size_t*>(inputBuffer.data() + sizeof(fileID));
+
+        ShapeDescriptor::cpu::array<DescriptorType> images(imageCount);
+
+        const size_t headerSize = 5 * sizeof(char) + sizeof(size_t);
+        const DescriptorType* imagesBasePointer = reinterpret_cast<const DescriptorType*>(inputBuffer.data() + headerSize);
+
+        std::copy(imagesBasePointer, imagesBasePointer + imageCount, images.content);
+
+        return images;
     }
 
 

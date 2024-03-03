057a5328c2a2fe21986f15144d8c6623d9d5ca49
Bart van Blokland <bartvblokl@gmail.com>
2024-03-03 16:26:22 +0100

    Catch2 version bump

--- tests/3dscTests.cpp
+++ tests/3dscTests.cpp
@@ -1,4 +1,4 @@
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
 #include <shapeDescriptor/shapeDescriptor.h>
 
 TEST_CASE("Ensuring volume computation makes sense") {
--- tests/pointDensities.cpp
+++ tests/pointDensities.cpp
@@ -1,4 +1,4 @@
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
 #ifdef DESCRIPTOR_CUDA_KERNELS_ENABLED
 #include <cuda_runtime.h>
 #include <vector_types.h>

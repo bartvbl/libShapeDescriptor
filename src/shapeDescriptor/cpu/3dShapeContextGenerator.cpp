f17e652a8c4070ccb0c8bca00581a5be73cd572f
bartvbl (TURBONINJA) <bartvblokl@gmail.com>
2024-03-03 15:56:03 +0100

    Preliminary CPU 3DSC implementation

--- src/shapeDescriptor/cpu/3dShapeContextGenerator.cpp
+++ src/shapeDescriptor/cpu/3dShapeContextGenerator.cpp
@@ -12,10 +12,7 @@ const size_t elementsPerShapeContextDescriptor =
         SHAPE_CONTEXT_VERTICAL_SLICE_COUNT *
         SHAPE_CONTEXT_LAYER_COUNT;
 
-#ifdef DESCRIPTOR_CUDA_KERNELS_ENABLED
-__host__ __device__
-#endif
-__inline__ float computeLayerDistance(float minSupportRadius, float maxSupportRadius, short layerIndex) {
+float computeLayerDistance(float minSupportRadius, float maxSupportRadius, short layerIndex) {
     // Avoiding zero divisions
     if(minSupportRadius == 0) {
         minSupportRadius = 0.000001f;
@@ -26,10 +23,8 @@ __inline__ float computeLayerDistance(float minSupportRadius, float maxSupportRa
             * std::log(float(maxSupportRadius) / float(minSupportRadius))));
 }
 
-#ifdef DESCRIPTOR_CUDA_KERNELS_ENABLED
-__host__ __device__
-#endif
-__inline__ float computeWedgeSegmentVolume(short verticalBinIndex, float radius) {
+
+inline float computeWedgeSegmentVolume(short verticalBinIndex, float radius) {
     const float verticalAngleStep = 1.0f / float(SHAPE_CONTEXT_VERTICAL_SLICE_COUNT);
     float binStartAngle = float(verticalBinIndex) * verticalAngleStep;
     float binEndAngle = float(verticalBinIndex + 1) * verticalAngleStep;
@@ -39,9 +34,6 @@ __inline__ float computeWedgeSegmentVolume(short verticalBinIndex, float radius)
     return scaleFraction * (std::cos(binStartAngle) - std::cos(binEndAngle));
 }
 
-#ifdef DESCRIPTOR_CUDA_KERNELS_ENABLED
-__host__ __device__
-#endif
 inline float computeSingleBinVolume(short verticalBinIndex, short layerIndex, float minSupportRadius, float maxSupportRadius) {
     // The wedge segment computation goes all the way from the center to the edge of the sphere
     // Since we also have a minimum support radius, we need to cut out the volume of the centre part
@@ -60,164 +52,152 @@ inline float computeSingleBinVolume(short verticalBinIndex, short layerIndex, fl
 float ShapeDescriptor::internal::computeBinVolume(short verticalBinIndex, short layerIndex, float minSupportRadius, float maxSupportRadius) {
     return computeSingleBinVolume(verticalBinIndex, layerIndex, minSupportRadius, maxSupportRadius);
 }
-#ifdef DESCRIPTOR_CUDA_KERNELS_ENABLED
 
-__device__ float absoluteAngle(float y, float x) {
+float absoluteAngle(float y, float x) {
     float absoluteAngle = std::atan2(y, x);
     return absoluteAngle < 0 ? absoluteAngle + (2.0f * float(M_PI)) : absoluteAngle;
 }
 
 // Run once for every vertex index
-__global__ void createDescriptors(
-        ShapeDescriptor::OrientedPoint* device_spinImageOrigins,
-        ShapeDescriptor::gpu::PointCloud pointCloud,
-        ShapeDescriptor::gpu::array<ShapeDescriptor::ShapeContextDescriptor> descriptors,
-        ShapeDescriptor::gpu::array<unsigned int> pointDensityArray,
-        size_t sampleCount,
-        float minSupportRadius,
-        float maxSupportRadius)
+void createDescriptors(
+    ShapeDescriptor::cpu::array<ShapeDescriptor::OrientedPoint> spinImageOrigins,
+    ShapeDescriptor::cpu::PointCloud pointCloud,
+    ShapeDescriptor::cpu::array<ShapeDescriptor::ShapeContextDescriptor> descriptors,
+    ShapeDescriptor::cpu::array<unsigned int> pointDensityArray,
+    size_t sampleCount,
+    float minSupportRadius,
+    float maxSupportRadius)
 {
-#define descriptorIndex blockIdx.x
+    for(uint32_t descriptorIndex = 0; descriptorIndex < pointCloud.pointCount; descriptorIndex++) {
 
-    const ShapeDescriptor::OrientedPoint spinOrigin = device_spinImageOrigins[descriptorIndex];
+        const ShapeDescriptor::OrientedPoint spinOrigin = spinImageOrigins[descriptorIndex];
 
-    const float3 vertex = spinOrigin.vertex;
-    float3 normal = spinOrigin.normal;
+        const ShapeDescriptor::cpu::float3 vertex = spinOrigin.vertex;
+        ShapeDescriptor::cpu::float3 normal = spinOrigin.normal;
 
-    normal /= length(normal);
+        normal = normalize(normal);
 
-    __shared__ ShapeDescriptor::ShapeContextDescriptor localDescriptor;
-    for(int i = threadIdx.x; i < elementsPerShapeContextDescriptor; i += blockDim.x) {
-        localDescriptor.contents[i] = 0;
-    }
-
-    __syncthreads();
-
-    // First, we align the input vertex with the descriptor's coordinate system
-    float3 arbitraryAxis = {0, 0, 1};
-    if(normal == arbitraryAxis || -normal == arbitraryAxis) {
-        arbitraryAxis = {1, 0, 0};
-    }
-
-    float3 referenceXAxis = cross(arbitraryAxis, normal);
-    float3 referenceYAxis = cross(referenceXAxis, normal);
-
-    assert(length(referenceXAxis) != 0);
-    assert(length(referenceYAxis) != 0);
-
-    referenceXAxis /= length(referenceXAxis);
-    referenceYAxis /= length(referenceYAxis);
-
-    for (unsigned int sampleIndex = threadIdx.x; sampleIndex < sampleCount; sampleIndex += blockDim.x) {
-        // 0. Fetch sample vertex
-
-        const float3 samplePoint = pointCloud.vertices.at(sampleIndex);
-
-        // 1. Compute bin indices
-
-        const float3 translated = samplePoint - vertex;
-
-        // Only include vertices which are within the support radius
-        float distanceToVertex = length(translated);
-        if (distanceToVertex < minSupportRadius || distanceToVertex > maxSupportRadius) {
-            continue;
+        for(float & content : descriptors.content[descriptorIndex].contents) {
+            content = 0;
         }
 
-        // Transforming descriptor coordinate system to the origin
-        // In the new system, 'z' is 'up'
-        const float3 relativeSamplePoint = {
-                referenceXAxis.x * translated.x + referenceXAxis.y * translated.y + referenceXAxis.z * translated.z,
-                referenceYAxis.x * translated.x + referenceYAxis.y * translated.y + referenceYAxis.z * translated.z,
-                normal.x * translated.x + normal.y * translated.y + normal.z * translated.z,
-        };
+        // First, we align the input vertex with the descriptor's coordinate system
+        ShapeDescriptor::cpu::float3 arbitraryAxis = {0, 0, 1};
+        if (normal == arbitraryAxis || -1 * normal == arbitraryAxis) {
+            arbitraryAxis = {1, 0, 0};
+        }
 
-        float2 horizontalDirection = {relativeSamplePoint.x, relativeSamplePoint.y};
-        float2 verticalDirection = {length(horizontalDirection), relativeSamplePoint.z};
+        ShapeDescriptor::cpu::float3 referenceXAxis = cross(arbitraryAxis, normal);
+        ShapeDescriptor::cpu::float3 referenceYAxis = cross(referenceXAxis, normal);
 
-        if (horizontalDirection == make_float2(0, 0)) {
-            // special case, will result in an angle of 0
-            horizontalDirection = {1, 0};
+        assert(length(referenceXAxis) != 0);
+        assert(length(referenceYAxis) != 0);
 
-            // Vertical direction is only 0 if all components are 0
-            // Should theoretically never occur, but let's handle it just in case
-            if (verticalDirection.y == 0) {
-                verticalDirection = {1, 0};
+        referenceXAxis = normalize(referenceXAxis);
+        referenceYAxis = normalize(referenceYAxis);
+
+        for (unsigned int sampleIndex = 0; sampleIndex < sampleCount; sampleIndex++) {
+            // 0. Fetch sample vertex
+
+            const ShapeDescriptor::cpu::float3 samplePoint = pointCloud.vertices[sampleIndex];
+
+            // 1. Compute bin indices
+
+            const ShapeDescriptor::cpu::float3 translated = samplePoint - vertex;
+
+            // Only include vertices which are within the support radius
+            float distanceToVertex = length(translated);
+            if (distanceToVertex < minSupportRadius || distanceToVertex > maxSupportRadius) {
+                continue;
             }
-        }
 
-        // normalise direction vectors
-        horizontalDirection /= length(horizontalDirection);
-        verticalDirection /= length(verticalDirection);
+            // Transforming descriptor coordinate system to the origin
+            // In the new system, 'z' is 'up'
+            const ShapeDescriptor::cpu::float3 relativeSamplePoint = {
+                    referenceXAxis.x * translated.x + referenceXAxis.y * translated.y + referenceXAxis.z * translated.z,
+                    referenceYAxis.x * translated.x + referenceYAxis.y * translated.y + referenceYAxis.z * translated.z,
+                    normal.x * translated.x + normal.y * translated.y + normal.z * translated.z,
+            };
 
-        float horizontalAngle = absoluteAngle(horizontalDirection.y, horizontalDirection.x);
-        short horizontalIndex =
-                unsigned((horizontalAngle / (2.0f * float(M_PI))) *
-                         float(SHAPE_CONTEXT_HORIZONTAL_SLICE_COUNT))
-                % SHAPE_CONTEXT_HORIZONTAL_SLICE_COUNT;
+            ShapeDescriptor::cpu::float2 horizontalDirection = {relativeSamplePoint.x, relativeSamplePoint.y};
+            ShapeDescriptor::cpu::float2 verticalDirection = {length(horizontalDirection), relativeSamplePoint.z};
 
-        float verticalAngle = std::fmod(absoluteAngle(verticalDirection.y, verticalDirection.x) + (float(M_PI) / 2.0f),
-                                        2.0f * float(M_PI));
-        short verticalIndex =
-                unsigned((verticalAngle / M_PI) *
-                         float(SHAPE_CONTEXT_VERTICAL_SLICE_COUNT))
-                % SHAPE_CONTEXT_VERTICAL_SLICE_COUNT;
+            if (horizontalDirection == ShapeDescriptor::cpu::float2(0, 0)) {
+                // special case, will result in an angle of 0
+                horizontalDirection = {1, 0};
 
-        float sampleDistance = length(relativeSamplePoint);
-        short layerIndex = 0;
-
-        // Recomputing logarithms is still preferable over doing memory transactions for each of them
-        for (; layerIndex < SHAPE_CONTEXT_LAYER_COUNT; layerIndex++) {
-            float nextSliceEnd = computeLayerDistance(minSupportRadius, maxSupportRadius, layerIndex + 1);
-            if (sampleDistance <= nextSliceEnd) {
-                break;
+                // Vertical direction is only 0 if all components are 0
+                // Should theoretically never occur, but let's handle it just in case
+                if (verticalDirection.y == 0) {
+                    verticalDirection = {1, 0};
+                }
             }
+
+            // normalise direction vectors
+            horizontalDirection = normalize(horizontalDirection);
+            verticalDirection = normalize(verticalDirection);
+
+            float horizontalAngle = absoluteAngle(horizontalDirection.y, horizontalDirection.x);
+            short horizontalIndex =
+                    unsigned((horizontalAngle / (2.0f * float(M_PI))) *
+                             float(SHAPE_CONTEXT_HORIZONTAL_SLICE_COUNT))
+                    % SHAPE_CONTEXT_HORIZONTAL_SLICE_COUNT;
+
+            float verticalAngle = std::fmod(
+                    absoluteAngle(verticalDirection.y, verticalDirection.x) + (float(M_PI) / 2.0f),
+                    2.0f * float(M_PI));
+            short verticalIndex =
+                    unsigned((verticalAngle / M_PI) *
+                             float(SHAPE_CONTEXT_VERTICAL_SLICE_COUNT))
+                    % SHAPE_CONTEXT_VERTICAL_SLICE_COUNT;
+
+            float sampleDistance = length(relativeSamplePoint);
+            short layerIndex = 0;
+
+            // Recomputing logarithms is still preferable over doing memory transactions for each of them
+            for (; layerIndex < SHAPE_CONTEXT_LAYER_COUNT; layerIndex++) {
+                float nextSliceEnd = computeLayerDistance(minSupportRadius, maxSupportRadius, layerIndex + 1);
+                if (sampleDistance <= nextSliceEnd) {
+                    break;
+                }
+            }
+
+            // Rounding errors can cause it to exceed its allowed bounds in specific cases
+            // Of course, on the off chance something is wrong after all,
+            // the assertions further down should trip. So we only handle the single
+            // edge case where layerIndex went one over.
+            if (layerIndex == SHAPE_CONTEXT_LAYER_COUNT) {
+                layerIndex--;
+            }
+
+            short3 binIndex = {horizontalIndex, verticalIndex, layerIndex};
+            assert(binIndex.x >= 0);
+            assert(binIndex.y >= 0);
+            assert(binIndex.z >= 0);
+            assert(binIndex.x < SHAPE_CONTEXT_HORIZONTAL_SLICE_COUNT);
+            assert(binIndex.y < SHAPE_CONTEXT_VERTICAL_SLICE_COUNT);
+            assert(binIndex.z < SHAPE_CONTEXT_LAYER_COUNT);
+
+            // 2. Compute sample weight
+            float binVolume = computeSingleBinVolume(binIndex.y, binIndex.z, minSupportRadius, maxSupportRadius);
+
+            // Volume can't be 0, and should be less than the volume of the support volume
+            assert(binVolume > 0);
+            assert(binVolume < (4.0f / 3.0f) * M_PI * maxSupportRadius * maxSupportRadius * maxSupportRadius);
+
+            float sampleWeight = 1.0f / (pointDensityArray.content[sampleIndex] * std::cbrt(binVolume));
+
+            // 3. Increment appropriate bin
+            unsigned int index =
+                    binIndex.x * SHAPE_CONTEXT_LAYER_COUNT * SHAPE_CONTEXT_VERTICAL_SLICE_COUNT +
+                    binIndex.y * SHAPE_CONTEXT_LAYER_COUNT +
+                    binIndex.z;
+            assert(index < elementsPerShapeContextDescriptor);
+            assert(!isnan(sampleWeight));
+            descriptors.content[descriptorIndex].contents[index] += sampleWeight;
         }
-
-        // Rounding errors can cause it to exceed its allowed bounds in specific cases
-        // Of course, on the off chance something is wrong after all,
-        // the assertions further down should trip. So we only handle the single
-        // edge case where layerIndex went one over.
-        if(layerIndex == SHAPE_CONTEXT_LAYER_COUNT) {
-            layerIndex--;
-        }
-
-        short3 binIndex = {horizontalIndex, verticalIndex, layerIndex};
-        assert(binIndex.x >= 0);
-        assert(binIndex.y >= 0);
-        assert(binIndex.z >= 0);
-        assert(binIndex.x < SHAPE_CONTEXT_HORIZONTAL_SLICE_COUNT);
-        assert(binIndex.y < SHAPE_CONTEXT_VERTICAL_SLICE_COUNT);
-        assert(binIndex.z < SHAPE_CONTEXT_LAYER_COUNT);
-
-        // 2. Compute sample weight
-        float binVolume = computeSingleBinVolume(binIndex.y, binIndex.z, minSupportRadius, maxSupportRadius);
-
-        // Volume can't be 0, and should be less than the volume of the support volume
-        assert(binVolume > 0);
-        assert(binVolume < (4.0f / 3.0f) * M_PI * maxSupportRadius * maxSupportRadius * maxSupportRadius);
-
-        float sampleWeight = 1.0f / (pointDensityArray.content[sampleIndex] * std::cbrt(binVolume));
-
-        // 3. Increment appropriate bin
-        unsigned int index =
-                binIndex.x * SHAPE_CONTEXT_LAYER_COUNT * SHAPE_CONTEXT_VERTICAL_SLICE_COUNT +
-                binIndex.y * SHAPE_CONTEXT_LAYER_COUNT +
-                binIndex.z;
-        assert(index < elementsPerShapeContextDescriptor);
-        assert(!isnan(sampleWeight));
-        atomicAdd(&localDescriptor.contents[index], sampleWeight);
     }
-
-    __syncthreads();
-
-    // Copy final descriptor into memory
-
-    for(size_t i = threadIdx.x; i < elementsPerShapeContextDescriptor; i += blockDim.x) {
-        descriptors.content[descriptorIndex].contents[i] = localDescriptor.contents[i];
-    }
-
 }
-#endif
 
 ShapeDescriptor::cpu::array<ShapeDescriptor::ShapeContextDescriptor> ShapeDescriptor::generate3DSCDescriptors(
         ShapeDescriptor::cpu::PointCloud pointCloud,
@@ -242,16 +222,14 @@ ShapeDescriptor::cpu::array<ShapeDescriptor::ShapeContextDescriptor> ShapeDescri
     // -- Spin Image Generation --
     auto generationStart = std::chrono::steady_clock::now();
 
-    createDescriptors <<<descriptorCount COMMA 416>>>(
-        device_spinImageOrigins.content,
-        device_pointCloud,
-        device_descriptors,
-        device_pointCountArray,
-        device_pointCloud.pointCount,
+    createDescriptors(
+        imageOrigins,
+        pointCloud,
+        descriptors,
+        pointCountArray,
+        pointCloud.pointCount,
         minSupportRadius,
         maxSupportRadius);
-    checkCudaErrors(cudaDeviceSynchronize());
-    checkCudaErrors(cudaGetLastError());
 
     std::chrono::milliseconds generationDuration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - generationStart);
 

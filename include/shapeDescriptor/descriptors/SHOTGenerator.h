76b08e5dcd3b8b51a7d872bbf657e31e754fe100
bartvbl <bartvblokl@gmail.com>
2024-05-24 17:24:54 +0200

    Some work on SHOT

--- include/shapeDescriptor/descriptors/SHOTGenerator.h
+++ include/shapeDescriptor/descriptors/SHOTGenerator.h
@@ -7,6 +7,21 @@ namespace ShapeDescriptor {
 
     namespace internal {
         template<uint32_t ELEVATION_DIVISIONS = 2, uint32_t RADIAL_DIVISIONS = 2, uint32_t AZIMUTH_DIVISIONS = 8, uint32_t INTERNAL_HISTOGRAM_BINS = 11>
+        inline void incrementSHOTBin(ShapeDescriptor::SHOTDescriptor<ELEVATION_DIVISIONS, RADIAL_DIVISIONS, AZIMUTH_DIVISIONS, INTERNAL_HISTOGRAM_BINS>& descriptor, uint32_t elevationBinIndex, uint32_t radialBinIndex, uint32_t azimuthBinIndex, uint32_t histogramBinIndex, float contribution) {
+            assert(elevationBinIndex < ELEVATION_DIVISIONS);
+            assert(radialBinIndex < RADIAL_DIVISIONS);
+            assert(azimuthBinIndex < AZIMUTH_DIVISIONS);
+            assert(histogramBinIndex < INTERNAL_HISTOGRAM_BINS);
+            uint32_t descriptorBinIndex =
+                    elevationBinIndex * RADIAL_DIVISIONS * AZIMUTH_DIVISIONS * INTERNAL_HISTOGRAM_BINS
+                  + radialBinIndex * AZIMUTH_DIVISIONS * INTERNAL_HISTOGRAM_BINS
+                  + azimuthBinIndex * INTERNAL_HISTOGRAM_BINS
+                  + histogramBinIndex;
+            assert(descriptorBinIndex < ELEVATION_DIVISIONS * RADIAL_DIVISIONS * AZIMUTH_DIVISIONS * INTERNAL_HISTOGRAM_BINS);
+            descriptor.contents[descriptorBinIndex] += contribution;
+        }
+
+        template<uint32_t ELEVATION_DIVISIONS = 2, uint32_t RADIAL_DIVISIONS = 2, uint32_t AZIMUTH_DIVISIONS = 8, uint32_t INTERNAL_HISTOGRAM_BINS = 11>
         void computeGeneralisedSHOTDescriptor(
                 const ShapeDescriptor::cpu::array<ShapeDescriptor::OrientedPoint> descriptorOrigins,
                 const ShapeDescriptor::cpu::PointCloud pointCloud,
@@ -18,6 +33,7 @@ namespace ShapeDescriptor {
 
             for(uint32_t descriptorIndex = 0; descriptorIndex < descriptors.length; descriptorIndex++) {
                 const ShapeDescriptor::cpu::float3 vertex = descriptorOrigins.content[descriptorIndex].vertex;
+                const ShapeDescriptor::cpu::float3 normal = descriptorOrigins.content[descriptorIndex].normal;
 
                 for(float & content : descriptors.content[descriptorIndex].contents) {
                     content = 0;
@@ -28,6 +44,7 @@ namespace ShapeDescriptor {
 
                     const ShapeDescriptor::cpu::float3 samplePoint = pointCloud.vertices[sampleIndex];
 
+
                     // 1. Compute bin indices
                     const ShapeDescriptor::cpu::float3 translated = samplePoint - vertex;
 
@@ -37,12 +54,17 @@ namespace ShapeDescriptor {
                         continue;
                     }
 
+
                     // Transforming descriptor coordinate system to the origin
                     const ShapeDescriptor::cpu::float3 relativeSamplePoint = {
                             dot(localReferenceFrames.at(descriptorIndex).xAxis, translated),
                             dot(localReferenceFrames.at(descriptorIndex).yAxis, translated),
                             dot(localReferenceFrames.at(descriptorIndex).zAxis, translated)
                     };
+
+
+
+
 
                     ShapeDescriptor::cpu::float2 horizontalDirection = {relativeSamplePoint.x, relativeSamplePoint.y};
                     ShapeDescriptor::cpu::float2 verticalDirection = {length(horizontalDirection), relativeSamplePoint.z};
@@ -62,8 +84,49 @@ namespace ShapeDescriptor {
                     horizontalDirection = normalize(horizontalDirection);
                     verticalDirection = normalize(verticalDirection);
 
-                    float horizontalAngle = internal::absoluteAngle(horizontalDirection.y, horizontalDirection.x);
-                    uint32_t horizontalIndex = unsigned((horizontalAngle / (2.0f * float(M_PI))) * float(AZIMUTH_DIVISIONS)) % AZIMUTH_DIVISIONS;
+                    const ShapeDescriptor::cpu::float3 sampleNormal = normalize(pointCloud.normals[sampleIndex]);
+                    float normalCosine = dot(sampleNormal, normalize(normal));
+
+                    // For the interpolations we'll use the order used in the paper
+                    // a) Interpolation on normal cosines
+                    float cosineHistogramPosition = clamp(((0.5f * normalCosine) + 0.5f) * float(INTERNAL_HISTOGRAM_BINS), 0.0f, float(INTERNAL_HISTOGRAM_BINS));
+                    uint32_t cosineHistogramBinIndex = std::min(INTERNAL_HISTOGRAM_BINS - 1, uint32_t(cosineHistogramPosition));
+                    float cosineHistogramDelta = cosineHistogramPosition - (float(cosineHistogramBinIndex) + 0.5f);
+                    uint32_t cosineHistogramNeighbourBinIndex;
+                    if(cosineHistogramDelta >= 0) {
+                        cosineHistogramNeighbourBinIndex = (cosineHistogramBinIndex + 1) % INTERNAL_HISTOGRAM_BINS;
+                    } else if(cosineHistogramDelta < 0 && cosineHistogramBinIndex > 0) {
+                        cosineHistogramNeighbourBinIndex = cosineHistogramBinIndex - 1;
+                    } else {
+                        cosineHistogramNeighbourBinIndex = INTERNAL_HISTOGRAM_BINS - 1;
+                    }
+                    float cosineHistogramBinContribution = std::abs(cosineHistogramDelta);
+                    float cosineHistogramNeighbourBinContribution = 1.0f - cosineHistogramBinContribution;
+
+
+                    // b) Interpolation on azimuth
+                    float azimuthAnglePosition = (internal::absoluteAngle(horizontalDirection.y, horizontalDirection.x) / (2.0f * float(M_PI))) * float(AZIMUTH_DIVISIONS);
+                    if(azimuthAnglePosition < 0) {
+                        azimuthAnglePosition += float(AZIMUTH_DIVISIONS);
+                    } else if(azimuthAnglePosition >= float(AZIMUTH_DIVISIONS)) {
+                        azimuthAnglePosition -= float(AZIMUTH_DIVISIONS);
+                    }
+                    uint32_t azimuthBinIndex = std::min(AZIMUTH_DIVISIONS - 1, uint32_t(azimuthAnglePosition));
+                    float azimuthHistogramDelta = azimuthAnglePosition - (float(azimuthBinIndex) + 0.5f);
+                    uint32_t azimuthNeighbourBinIndex;
+                    if(azimuthHistogramDelta >= 0) {
+                        azimuthNeighbourBinIndex = (azimuthBinIndex + 1) % AZIMUTH_DIVISIONS;
+                    } else if(azimuthHistogramDelta < 0 && azimuthBinIndex > 0) {
+                        azimuthNeighbourBinIndex = azimuthBinIndex - 1;
+                    } else {
+                        azimuthNeighbourBinIndex = AZIMUTH_DIVISIONS - 1;
+                    }
+                    float azimuthBinContribution = std::abs(azimuthHistogramDelta);
+                    float azimuthNeighbourBinContribution = 1.0f - azimuthBinContribution;
+
+
+                    // c) Interpolation on elevation
+
 
                     float verticalAngle = std::fmod(
                             internal::absoluteAngle(verticalDirection.y, verticalDirection.x) + (float(M_PI) / 2.0f),
@@ -78,6 +141,8 @@ namespace ShapeDescriptor {
                     assert(binIndex.x < AZIMUTH_DIVISIONS);
                     assert(binIndex.y < ELEVATION_DIVISIONS);
                     assert(binIndex.z < RADIAL_DIVISIONS);
+
+
 
 
                     // 3. Increment appropriate bin
@@ -107,14 +172,14 @@ namespace ShapeDescriptor {
         return descriptors;
     }
 
-    template<typename SHOTDescriptor>
-    cpu::array<SHOTDescriptor> generateSHOTDescriptors(
+    template<uint32_t ELEVATION_DIVISIONS = 2, uint32_t RADIAL_DIVISIONS = 2, uint32_t AZIMUTH_DIVISIONS = 8, uint32_t INTERNAL_HISTOGRAM_BINS = 11>
+    cpu::array<ShapeDescriptor::SHOTDescriptor<ELEVATION_DIVISIONS, RADIAL_DIVISIONS, AZIMUTH_DIVISIONS, INTERNAL_HISTOGRAM_BINS>> generateSHOTDescriptors(
             cpu::PointCloud cloud,
             cpu::array<OrientedPoint> descriptorOrigins,
             float supportRadius,
             SHOTExecutionTimes* executionTimes = nullptr) {
         std::vector<float> radii(descriptorOrigins.length, supportRadius);
 
-        return generateSHOTDescriptorsMultiRadius<SHOTDescriptor>(cloud, descriptorOrigins, radii, executionTimes);
+        return generateSHOTDescriptorsMultiRadius<ShapeDescriptor::SHOTDescriptor<ELEVATION_DIVISIONS, RADIAL_DIVISIONS, AZIMUTH_DIVISIONS, INTERNAL_HISTOGRAM_BINS>>(cloud, descriptorOrigins, radii, executionTimes);
     }
 }
